0a28b040e00bace43259c5c7aab4d96124cb4494
Tom Schindl <tom.schindl@bestsolution.at>
2013-04-13 13:52:33 +0200

    initial checkin

---
+++ bundles/runtime/org.eclipse.fx.services/src/org/eclipse/fx/services/theme/Theme.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.services.theme;
+
+import java.net.URL;
+import java.util.List;
+
+public interface Theme {
+	public String getId();
+	public String getName();
+	public List<URL> getStylesheetURL();
+}
