763102dcb788854015283fdc35765c8275d1036a
Tom Schindl <tom.schindl@bestsolution.at>
2013-04-19 13:24:54 +0200

    added missing class

---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/Util.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core;
+
+public class Util {
+	public static boolean isFX2() {
+		return System.getProperty("javafx.version") != null && System.getProperty("javafx.version").startsWith("2");
+	}
+}
