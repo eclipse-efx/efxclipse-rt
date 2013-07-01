1b5f70eb25181e931ca178c2039f1f6776a24ae0
Tom Schindl <tom.schindl@bestsolution.at>
2013-07-01 23:02:28 +0200

    Bug 408419 - Add FXML to Java Translator

---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/fxml/ExtendedFXMLLoader.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.fxml;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+
+
+public class ExtendedFXMLLoader {
+	public <T> T load(ClassLoader cl, String path) throws IOException {
+		try {
+			Class<?> clazz = cl.loadClass(path.replace('/', '.'));
+			FXMLDocument<T> d = (FXMLDocument<T>) clazz.newInstance();
+			return d.load(null);
+		} catch (ClassNotFoundException e) {
+			return FXMLLoader.load(cl.getResource(path));
+		} catch (InstantiationException e) {
+			throw new IOException(e);
+		} catch (IllegalAccessException e) {
+			throw new IOException(e);
+		}
+	}
+}
