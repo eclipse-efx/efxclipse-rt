25210c91cffccdcb014782499a3826d975cc929c
Tom Schindl <tom.schindl@bestsolution.at>
2013-07-18 16:57:05 +0200

    Bug 413254 - Add easy way for Keybinding Handlers to delegate to native implementation

---
+++ bundles/runtime/org.eclipse.fx.ui.services/src/org/eclipse/fx/ui/services/commands/NativeStrategy.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ui.services.commands;
+
+public enum NativeStrategy {
+	BLOCK,
+	PROCEED
+}
