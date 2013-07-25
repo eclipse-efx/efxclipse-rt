41c9c2eb5bd4d4647a51c5b1d41b4ef835c9fb53
Tom Schindl <tom.schindl@bestsolution.at>
2013-07-25 17:17:16 +0200

    initial check in

---
+++ testcases/org.eclipse.fx.testcases.fxml/src/at/bestsolution/efxclipse/testcases/fxml/MyController.java
@@ -0,0 +1,48 @@
+package at.bestsolution.efxclipse.testcases.fxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MyController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.err.println("The controller is created!!!");
+	}
+
+	/**
+	 * This the click 1 method
+	 * 
+	 * @param event
+	 *            the event
+	 */
+	@FXML
+	public void clicked(ActionEvent event) {
+		System.err.println("Hello World");
+	}
+
+	/**
+	 * This is the click 2 method
+	 * 
+	 * @param event
+	 *            the event
+	 */
+	@FXML
+	public void clicked2(ActionEvent event) {
+
+	}
+
+//	@FXML
+//	public void clicked(String event) {
+//
+//	}
+
+//	@FXML
+//	public void clicked() {
+//
+//	}
+}
