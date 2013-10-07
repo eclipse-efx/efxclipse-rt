820cf273ec658ef8b795d46fd9c904d1d48d8750
Tom Schindl <tom.schindl@bestsolution.at>
2013-10-07 21:39:31 +0200

    Bug 418850 - MDynamicMenuContributions does NOT work like in e4-swt

--- testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/contributions/DMContribution.java
+++ testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/contributions/DMContribution.java
@@ -1,21 +1,33 @@
 package org.eclipse.fx.testcases.e4.contributions;
 
+import java.util.List;
+
 import org.eclipse.e4.ui.di.AboutToHide;
 import org.eclipse.e4.ui.di.AboutToShow;
+import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuItem;
 
 public class DMContribution {
-	
-	
 	@AboutToShow
-	public void ats(MMenu menu){
+	public void ats(List<MMenuItem> items){
 //		menu.getChildren().remove(0); <--works
-		System.err.println(menu);
-		System.err.println("ATSH");
+//		System.err.println(menu);
+//		System.err.println("ATSH");
+		for(int i=0 ; i< 5; i++) {
+	           MDirectMenuItem menuItem = MMenuFactory.INSTANCE.createDirectMenuItem();
+	           menuItem.setLabel("Test  menu " + i);
+	           menuItem.setElementId("test.dynamicmenucontribution"+ i);
+	           menuItem.setContributorURI("platform:/plugin/org.eclipse.fx.testcases.e4");
+	           menuItem.setContributionURI("bundleclass://org.eclipse.fx.testcases.e4/org.eclipse.fx.testcases.e4.handlers.DiHandler");
+	           items.add(menuItem);
+	       }
 	}
 	
 	@AboutToHide
-	public void ath(){
+	public void ath(List<MMenuItem> items){
+		System.err.println(items.size());
 		System.err.println("ATH");
 	}
 
