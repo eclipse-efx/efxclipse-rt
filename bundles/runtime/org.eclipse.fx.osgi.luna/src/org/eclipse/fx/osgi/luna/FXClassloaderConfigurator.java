67996572916a6917f066cfa9f2e5ba128e2e0e0d
Tom Schindl <tom.schindl@bestsolution.at>
2013-11-08 16:21:47 +0100

    Bug 421141 - Runtime integration for Luna

--- bundles/runtime/org.eclipse.fx.osgi.luna/src/org/eclipse/fx/osgi/luna/FXClassloaderConfigurator.java
+++ bundles/runtime/org.eclipse.fx.osgi.luna/src/org/eclipse/fx/osgi/luna/FXClassloaderConfigurator.java
@@ -6,13 +6,8 @@ import org.eclipse.osgi.internal.hookregistry.HookRegistry;
 public class FXClassloaderConfigurator implements HookConfigurator {
 	public static final boolean DEBUG = true;
 
-	public FXClassloaderConfigurator() {
-		System.err.println("CLASSLOADING HOOK CONFIG CREATED");
-	}
-	
 	@Override
 	public void addHooks(HookRegistry hookRegistry) {
-		System.err.println("ADDING HOOK");
 		hookRegistry.addClassLoaderHook(new FXClassLoader());
 	}
 
