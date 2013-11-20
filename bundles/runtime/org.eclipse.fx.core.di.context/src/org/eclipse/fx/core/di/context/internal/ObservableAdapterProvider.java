95d5804c87be285e8bcba3e00b2af86e97ebb6f4
Tom Schindl <tom.schindl@bestsolution.at>
2013-11-21 00:05:35 +0100

    Bug 422198 - Provide more lightweight access to IEclipseContext values

---
+++ bundles/runtime/org.eclipse.fx.core.di.context/src/org/eclipse/fx/core/di/context/internal/ObservableAdapterProvider.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.di.context.internal;
+
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.IValueChangeListener;
+import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.fx.core.Callback;
+import org.eclipse.fx.core.adapter.AdapterProvider;
+import org.eclipse.fx.core.adapter.AdapterService.ValueAccess;
+import org.eclipse.fx.core.di.ContextBoundValue;
+
+/**
+ * Eclipse Databinding Observable Adapter
+ */
+@SuppressWarnings("rawtypes")
+public class ObservableAdapterProvider implements AdapterProvider<ContextBoundValue, IObservableValue> {
+
+	@Override
+	public Class<ContextBoundValue> getSourceType() {
+		return ContextBoundValue.class;
+	}
+
+	@Override
+	public Class<IObservableValue> getTargetType() {
+		return IObservableValue.class;
+	}
+
+	@Override
+	public boolean canAdapt(ContextBoundValue sourceObject, Class<IObservableValue> targetType) {
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public IObservableValue adapt(final ContextBoundValue sourceObject, Class<IObservableValue> targetType, ValueAccess... valueAccess) {
+		Realm r = null;
+		
+		for( ValueAccess a : valueAccess ) {
+			r = a.getValue(Realm.class);
+			if( r != null ) {
+				break;
+			}
+		}
+		
+		if( r == null ) {
+			r = Realm.getDefault();
+		}
+		
+		if( r == null ) {
+			r = new Realm() {
+				private Thread t = Thread.currentThread();
+				
+				@Override
+				public boolean isCurrent() {
+					return t == Thread.currentThread();
+				}
+			};
+		}
+		
+		final WritableValue w = new WritableValue(r);
+		w.setValue(sourceObject.getValue());
+		w.addValueChangeListener(new IValueChangeListener() {
+			
+			@Override
+			public void handleValueChange(ValueChangeEvent event) {
+				sourceObject.publish(event.diff.getNewValue());
+			}
+		});
+		sourceObject.subscribeOnValueChange(new Callback() {
+
+			@Override
+			public void call(Object value) {
+				w.setValue(value);
+			}
+		});
+		sourceObject.subscribeOnDispose(new Callback<Void>() {
+
+			@Override
+			public void call(Void value) {
+				w.dispose();
+			}
+		});
+		return w;
+	}
+}
---
+++ bundles/runtime/org.eclipse.fx.core.di/src/org/eclipse/fx/core/di/ContextValue.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.di;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ContextValue {
+	String contextKey();
+}
