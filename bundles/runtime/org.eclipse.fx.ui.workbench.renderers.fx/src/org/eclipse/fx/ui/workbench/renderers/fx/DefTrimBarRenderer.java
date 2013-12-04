3c74f21833bab2134c076a2fce6dd5fe8ad19086
Tom Schindl <tom.schindl@bestsolution.at>
2013-12-04 23:53:53 +0100

    Bug 423244 - Use VBox for rendering left and right trim bars

--- bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/DefTrimBarRenderer.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/DefTrimBarRenderer.java
@@ -10,35 +10,58 @@
  *******************************************************************************/
 package org.eclipse.fx.ui.workbench.renderers.fx;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
+import org.eclipse.e4.ui.model.application.ui.SideValue;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.fx.ui.workbench.renderers.base.BaseTrimBarRenderer;
 import org.eclipse.fx.ui.workbench.renderers.base.widget.WLayoutedWidget;
 import org.eclipse.fx.ui.workbench.renderers.base.widget.WTrimBar;
 import org.eclipse.fx.ui.workbench.renderers.fx.widget.WLayoutedWidgetImpl;
 
 
-public class DefTrimBarRenderer extends BaseTrimBarRenderer<HBox> {
+public class DefTrimBarRenderer extends BaseTrimBarRenderer<Pane> {
 
 	@Override
-	protected Class<? extends WTrimBar<HBox>> getWidgetClass(MTrimBar trimbar) {
+	protected Class<? extends WTrimBar<Pane>> getWidgetClass(MTrimBar trimbar) {
 		return WTrimBarImpl.class;
 	}
 
-	public static class WTrimBarImpl extends WLayoutedWidgetImpl<HBox, HBox, MTrimBar> implements WTrimBar<HBox> {
-
+	public static class WTrimBarImpl extends WLayoutedWidgetImpl<Pane, Pane, MTrimBar> implements WTrimBar<Pane> {
+		private SideValue sideValue;
+		
+		@Inject
+		public WTrimBarImpl(@Named(UIEvents.GenericTrimContainer.SIDE) SideValue sideValue) {
+			this.sideValue = sideValue;
+		}
+		
 		@Override
-		protected HBox createWidget() {
-			HBox box = new HBox();
-			return box;
+		protected Pane createWidget() {
+			//TODO Can we get dynamic here, what when the orientation changes
+			if( isVertical() ) {
+				VBox box = new VBox();
+				return box;
+			} else {
+				HBox box = new HBox();
+				return box;				
+			}
+		}
+		
+		private boolean isVertical() {
+			return sideValue == SideValue.LEFT || sideValue == SideValue.RIGHT;
 		}
 
 		@Override
-		protected HBox getWidgetNode() {
+		protected Pane getWidgetNode() {
 			return getWidget();
 		}
 		
@@ -46,7 +69,11 @@ public class DefTrimBarRenderer extends BaseTrimBarRenderer<HBox> {
 		public void addChild(WLayoutedWidget<MTrimElement> trimElementWidget) {
 			Node n = (Node) trimElementWidget.getStaticLayoutNode();
 			if( trimElementWidget.getDomElement().getTags().contains(TAG_FILLSPACE) ) {
-				HBox.setHgrow(n, Priority.ALWAYS);
+				if( isVertical() ) {
+					VBox.setVgrow(n, Priority.ALWAYS);
+				} else {
+					HBox.setHgrow(n, Priority.ALWAYS);	
+				}
 			}
 			getWidget().getChildren().add(n);
 		}
@@ -55,7 +82,11 @@ public class DefTrimBarRenderer extends BaseTrimBarRenderer<HBox> {
 		public void addChild(int idx, WLayoutedWidget<MTrimElement> trimElementWidget) {
 			Node n = (Node) trimElementWidget.getStaticLayoutNode();
 			if( trimElementWidget.getDomElement().getTags().contains(TAG_FILLSPACE) ) {
-				HBox.setHgrow(n, Priority.ALWAYS);
+				if( isVertical() ) {
+					VBox.setVgrow(n, Priority.ALWAYS); 
+				} else {
+					HBox.setHgrow(n, Priority.ALWAYS);	
+				}
 			}
 			getWidget().getChildren().add(idx, n);
 		}
