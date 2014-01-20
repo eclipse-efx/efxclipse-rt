8a24fb64e4a234ab4f604f9afe0fccbbade7a4dd
Tom Schindl <tom.schindl@bestsolution.at>
2014-01-20 11:16:28 +0100

    fixed copyright

--- bundles/runtime/org.eclipse.fx.ui.databinding/src/org/eclipse/fx/ui/databinding/ListUtil.java
+++ bundles/runtime/org.eclipse.fx.ui.databinding/src/org/eclipse/fx/ui/databinding/ListUtil.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.ui.databinding;
 
 import org.eclipse.core.databinding.observable.list.IObservableList;
@@ -7,34 +17,45 @@ import org.eclipse.fx.core.databinding.AdapterFactory;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
+/**
+ * Utility to setup {@link ListView} and {@link ComboBox}
+ */
 public class ListUtil {
+	/**
+	 * Setup a list where cell text is bound to the given property
+	 * 
+	 * @param listView
+	 *            the list view
+	 * @param property
+	 *            the text property
+	 */
 	public static <T> void setupList(ListView<T> listView, IValueProperty property) {
-		listView.setCellFactory(PropertyListCellFaytory.<T>textFactory(property));
+		listView.setCellFactory(PropertyListCellFaytory.<T> textFactory(property));
 	}
-	
+
 	public static <T> void setupList(ListView<T> listView, String template, IValueProperty... properties) {
-		listView.setCellFactory(PropertyListCellFaytory.<T>textFactory(template, properties));
+		listView.setCellFactory(PropertyListCellFaytory.<T> textFactory(template, properties));
 	}
-	
+
 	public static <T> void setupComboBox(ComboBox<T> comboBox, IValueProperty property) {
-		comboBox.setCellFactory(PropertyListCellFaytory.<T>textFactory(property)); 
-		comboBox.setButtonCell(PropertyListCellFaytory.<T>textCell(property));
+		comboBox.setCellFactory(PropertyListCellFaytory.<T> textFactory(property));
+		comboBox.setButtonCell(PropertyListCellFaytory.<T> textCell(property));
 	}
-	
+
 	public static <T> void setupComboBox(ComboBox<T> comboBox, String template, IValueProperty... properties) {
-		comboBox.setCellFactory(PropertyListCellFaytory.<T>textFactory(template,properties));
-		comboBox.setButtonCell(PropertyListCellFaytory.<T>textCell(template,properties));
+		comboBox.setCellFactory(PropertyListCellFaytory.<T> textFactory(template, properties));
+		comboBox.setButtonCell(PropertyListCellFaytory.<T> textCell(template, properties));
 	}
-	
+
 	public static <T> void setupComboBox(IObservableList list, ComboBox<T> comboBox, IValueProperty property) {
-		comboBox.setCellFactory(PropertyListCellFaytory.<T>textFactory(property));
-		comboBox.setButtonCell(PropertyListCellFaytory.<T>textCell(property));
-		comboBox.setItems(AdapterFactory.<T>adapt(list));
+		comboBox.setCellFactory(PropertyListCellFaytory.<T> textFactory(property));
+		comboBox.setButtonCell(PropertyListCellFaytory.<T> textCell(property));
+		comboBox.setItems(AdapterFactory.<T> adapt(list));
 	}
-	
+
 	public static <T> void setupComboBox(IObservableList list, ComboBox<T> comboBox, String template, IValueProperty... properties) {
-		comboBox.setCellFactory(PropertyListCellFaytory.<T>textFactory(template,properties));
-		comboBox.setButtonCell(PropertyListCellFaytory.<T>textCell(template,properties));
-		comboBox.setItems(AdapterFactory.<T>adapt(list));
+		comboBox.setCellFactory(PropertyListCellFaytory.<T> textFactory(template, properties));
+		comboBox.setButtonCell(PropertyListCellFaytory.<T> textCell(template, properties));
+		comboBox.setItems(AdapterFactory.<T> adapt(list));
 	}
 }
