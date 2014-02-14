921c251b18aa54535084cf9504e011d66bb1fc0b
Tom Schindl <tom.schindl@bestsolution.at>
2014-02-14 12:34:57 +0100

    remove javafx 2.2 support

--- bundles/runtime/org.eclipse.fx.emf.edit.ui/src/org/eclipse/fx/emf/edit/ui/CellUtil.java
+++ bundles/runtime/org.eclipse.fx.emf.edit.ui/src/org/eclipse/fx/emf/edit/ui/CellUtil.java
@@ -1,14 +1,15 @@
 package org.eclipse.fx.emf.edit.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.control.Cell;
 import javafx.scene.control.Control;
-import javafx.scene.control.ListView;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableRow;
@@ -20,58 +21,55 @@ import javafx.scene.control.TreeView;
  */
 public class CellUtil {
 
-	/**
-	 * Whether the JavaFX is version 2.x
-	 */
-	private static final boolean FX2 = System.getProperty("javafx.version") != null && System.getProperty("javafx.version").startsWith("2"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-
-	/**
-	 * Finds the row for a {@link Cell} so the feedback can be applied to the whole row
-	 * 
-	 * @param cell
-	 *            the cell
-	 * 
-	 * @return the containing {@link TableRow} for {@link TableCell}s or the {@link TreeTableRow} for
-	 *         {@link TreeTableCell}s and the {@link Cell} itself otherwise
-	 */
 	public static Cell<?> getRowNode(final Cell<?> cell) {
-		return FX2 ? CellUtil2.getRowNode(cell) : CellUtil8.getRowNode(cell);
+		if (cell instanceof TableCell)
+			return ((TableCell<?, ?>) cell).getTableRow();
+		else if (cell instanceof TreeTableCell)
+			return ((TreeTableCell<?, ?>) cell).getTreeTableRow();
+		else
+			return cell;
 	}
 
-	/**
-	 * Retrieves the {@link MultipleSelectionModel} from the {@link ListView}, {@link TreeView},
-	 * {@link TableView} or {@link TreeTableView} in which <code>cell</code> is displayed
-	 * 
-	 * @param cell
-	 *            the {@link Cell} for which the {@link MultipleSelectionModel} is to be retrieved
-	 * @return the {@link MultipleSelectionModel} for this {@link Cell}
-	 */
 	public static MultipleSelectionModel<?> getSelectionModel(Cell<?> cell) {
-		return FX2 ? CellUtil2.getSelectionModel(cell) : CellUtil8.getSelectionModel(cell);
+		if (cell instanceof ListCell)
+			return ((ListCell<?>) cell).getListView().getSelectionModel();
+		else if (cell instanceof TreeCell)
+			return ((TreeCell<?>) cell).getTreeView().getSelectionModel();
+		else if (cell instanceof TableCell)
+			return ((TableCell<?, ?>) cell).getTableView().getSelectionModel();
+		else if (cell instanceof TableRow)
+			return ((TableRow<?>) cell).getTableView().getSelectionModel();
+		else if (cell instanceof TreeTableCell)
+			return ((TreeTableCell<?, ?>) cell).getTreeTableView()
+					.getSelectionModel();
+		else if (cell instanceof TreeTableRow)
+			return ((TreeTableRow<?>) cell).getTreeTableView()
+					.getSelectionModel();
+		else
+			throw new IllegalArgumentException("Unsupported Cell type"); //$NON-NLS-1$
 	}
 
-	/**
-	 * Extract the selection model
-	 * 
-	 * @param view
-	 *            the view
-	 * @return the selection model
-	 */
-	public static MultipleSelectionModel<?> getSelectionModel(Control view) {
-		return FX2 ? CellUtil2.getSelectionModel(view) : CellUtil8.getSelectionModel(view);
+	static MultipleSelectionModel<?> getSelectionModel(Control view) {
+		if (view instanceof TreeView<?>)
+			return ((TreeView<?>) view).getSelectionModel();
+		else if (view instanceof TreeTableView<?>)
+			return ((TreeTableView<?>) view).getSelectionModel();
+		else
+			throw new IllegalArgumentException("Unsupported View type"); //$NON-NLS-1$
 	}
 
-	/**
-	 * Retrieves the selected items from the {@link ListView}, {@link TreeView}, {@link TableView} or
-	 * {@link TreeTableView} in which <code>cell</code> is displayed. {@link TreeItem}s will be replaced with
-	 * {@link TreeItem#getValue()}.
-	 * 
-	 * @param cell
-	 *            the {@link Cell} for which the selected items should be retrieved
-	 * @return a {@link List} with the selected items
-	 */
 	public static List<?> getSelectedItems(Cell<?> cell) {
-		return FX2 ? CellUtil2.getSelectedItems(cell) : CellUtil8.getSelectedItems(cell);
+		MultipleSelectionModel<?> selectionModel = getSelectionModel(cell);
+		List<?> items = selectionModel.getSelectedItems();
+
+		if (cell instanceof TreeCell || cell instanceof TreeTableCell) {
+			List<Object> unwrappedItems = new ArrayList<>(items.size());
+			for (Object item : items)
+				unwrappedItems.add(((TreeItem<?>) item).getValue());
+			return unwrappedItems;
+		}
+
+		return items;
 	}
 
 }
