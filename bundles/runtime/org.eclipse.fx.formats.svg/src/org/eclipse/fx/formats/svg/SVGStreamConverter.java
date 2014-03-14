c9fc6e3eff7eb36e9832b23870a565d2aa869155
Tom Schindl <tom.schindl@bestsolution.at>
2014-03-14 08:19:09 +0100

    fixed warnings

--- bundles/runtime/org.eclipse.fx.formats.svg/src/org/eclipse/fx/formats/svg/SVGStreamConverter.java
+++ bundles/runtime/org.eclipse.fx.formats.svg/src/org/eclipse/fx/formats/svg/SVGStreamConverter.java
@@ -18,9 +18,22 @@ import org.eclipse.fx.formats.svg.converter.FXMLConverter;
 import org.eclipse.fx.formats.svg.handler.XMLLoader;
 import org.eclipse.fx.formats.svg.svg.SvgSvgElement;
 
-
+/**
+ * Utility to convert an SVG file to FXML
+ */
 public class SVGStreamConverter {
-	public void converter(InputStream inputStream, OutputStream outputStream) throws Exception {
+	/**
+	 * Convert the svg stream to on FXML-Stream
+	 * 
+	 * @param inputStream
+	 *            the input stream (SVG)
+	 * @param outputStream
+	 *            the output string (FXML)
+	 * @throws Exception
+	 *             if an error occurs
+	 */
+	public static void converter(InputStream inputStream, OutputStream outputStream)
+			throws Exception {
 		XMLLoader loader = new XMLLoader();
 		SvgSvgElement rootElement = loader.loadDocument(null, inputStream);
 		FXMLConverter converter = new FXMLConverter(rootElement);
