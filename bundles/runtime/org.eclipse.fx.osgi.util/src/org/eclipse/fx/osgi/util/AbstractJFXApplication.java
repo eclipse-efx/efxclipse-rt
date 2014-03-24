2ededfe42f6fa2f53088e099c4ee78a0597a4470
Tom Schindl <tom.schindl@bestsolution.at>
2014-03-24 08:28:43 -0700

    improved javadoc

--- bundles/runtime/org.eclipse.fx.osgi.util/src/org/eclipse/fx/osgi/util/AbstractJFXApplication.java
+++ bundles/runtime/org.eclipse.fx.osgi.util/src/org/eclipse/fx/osgi/util/AbstractJFXApplication.java
@@ -19,6 +19,7 @@ import javafx.stage.Stage;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.fx.core.databinding.JFXRealm;
+import org.eclipse.jdt.annotation.NonNull;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
@@ -31,59 +32,65 @@ import org.osgi.service.event.EventAdmin;
  */
 public abstract class AbstractJFXApplication implements IApplication {
 	static AbstractJFXApplication SELF;
-	
+
 	IApplicationContext applicationContext;
 	Object returnValue;
 	EventAdmin eventAdmin;
-	
+
 	/**
 	 * Dummy class for bootstrap
 	 */
 	public static class JFXApp extends Application {
 		private AbstractJFXApplication osgiApp = SELF;
 		private IApplicationContext applicationContext;
-		
+
 		@Override
 		public void start(final Stage primaryStage) throws Exception {
 			this.applicationContext = this.osgiApp.applicationContext;
-			
+
 			JFXRealm.createDefault();
-			this.osgiApp.jfxStart(this.applicationContext,JFXApp.this,primaryStage);
-			
-			if( this.osgiApp.eventAdmin != null ) {
+			this.osgiApp.jfxStart(this.applicationContext, JFXApp.this,
+					primaryStage);
+
+			if (this.osgiApp.eventAdmin != null) {
 				Map<String, Object> map = new HashMap<String, Object>();
-//				map.put("name", value);
-				this.osgiApp.eventAdmin.sendEvent(new Event("efxapp/applicationLaunched", map)); //$NON-NLS-1$
+				// map.put("name", value);
+				this.osgiApp.eventAdmin.sendEvent(new Event(
+						"efxapp/applicationLaunched", map)); //$NON-NLS-1$
 			}
 		}
-		
+
 		@Override
 		public void stop() throws Exception {
 			super.stop();
 			this.osgiApp.returnValue = this.osgiApp.jfxStop();
 		}
 	}
-	
+
 	@Override
 	public final Object start(IApplicationContext context) throws Exception {
 		SELF = this;
 		this.applicationContext = context;
 		this.applicationContext.applicationRunning();
-		
+
 		Bundle b = FrameworkUtil.getBundle(AbstractJFXApplication.class);
 		BundleContext bundleContext = b.getBundleContext();
-		ServiceReference<EventAdmin> ref = bundleContext.getServiceReference(EventAdmin.class);
-		if( ref != null ) {
+		ServiceReference<EventAdmin> ref = bundleContext
+				.getServiceReference(EventAdmin.class);
+		if (ref != null) {
 			this.eventAdmin = bundleContext.getService(ref);
 		}
-		
-		// Looks like OS-X wants to have the context class loader to locate FX-Classes
-		Thread.currentThread().setContextClassLoader(Application.class.getClassLoader());
-		
+
+		// Looks like OS-X wants to have the context class loader to locate
+		// FX-Classes
+		Thread.currentThread().setContextClassLoader(
+				Application.class.getClassLoader());
+
 		Application.launch(JFXApp.class);
-		
+
 		try {
-			return this.returnValue == null ? IApplication.EXIT_OK : this.returnValue;
+			return this.returnValue == null ? IApplication.EXIT_OK
+					: this.returnValue;
 		} finally {
 			this.returnValue = null;
 		}
@@ -94,8 +101,30 @@ public abstract class AbstractJFXApplication implements IApplication {
 		// Nothing
 	}
 
-	protected abstract void jfxStart(IApplicationContext applicationContext, Application jfxApplication, Stage primaryStage);
-	
+	/**
+	 * Method called when OSGi & JavaFX are fully initialized
+	 * {@link Application#start(Stage)}
+	 * 
+	 * @param applicationContext
+	 *            the application context
+	 * @param jfxApplication
+	 *            the javafx application instance
+	 * @param primaryStage
+	 * @see IApplication#start(IApplicationContext)
+	 * @see Application#start(Stage)
+	 */
+	protected abstract void jfxStart(
+			@NonNull IApplicationContext applicationContext,
+			@NonNull Application jfxApplication, @NonNull Stage primaryStage);
+
+	/**
+	 * Method called when the javafx application is shutdown
+	 * {@link Application#stop()}
+	 * 
+	 * @return the return status for the OSGi application see {@link IApplication#start(IApplicationContext)}
+	 * @see IApplication#start(IApplicationContext)
+	 * @see Application#stop()
+	 */
 	@SuppressWarnings("static-method")
 	protected Object jfxStop() {
 		return IApplication.EXIT_OK;
