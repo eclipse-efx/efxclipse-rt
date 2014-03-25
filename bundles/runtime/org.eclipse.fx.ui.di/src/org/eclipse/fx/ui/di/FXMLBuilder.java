69b2cf94d34fe85914c3231bb958aa163e645b29
Tom Schindl <tom.schindl@bestsolution.at>
2014-03-25 19:30:57 +0100

    fixed javadoc

--- bundles/runtime/org.eclipse.fx.ui.di/src/org/eclipse/fx/ui/di/FXMLBuilder.java
+++ bundles/runtime/org.eclipse.fx.ui.di/src/org/eclipse/fx/ui/di/FXMLBuilder.java
@@ -13,16 +13,75 @@ package org.eclipse.fx.ui.di;
 import java.io.IOException;
 import java.util.ResourceBundle;
 
+import org.eclipse.jdt.annotation.NonNull;
+
 import javafx.util.BuilderFactory;
 
+/**
+ * Builder for an FXMLLoader
+ * 
+ * @param <N>
+ *            the root type loaded by the builder
+ */
 public interface FXMLBuilder<N> {
-	FXMLBuilder<N> resourceBundle(ResourceBundle resourceBundle);
+	/**
+	 * Associate a resource bundle with the loader
+	 * 
+	 * @param resourceBundle
+	 *            the resource bundle
+	 * @return the builder instance
+	 */
+	@NonNull
+	FXMLBuilder<N> resourceBundle(@NonNull ResourceBundle resourceBundle);
+
+	/**
+	 * Associate a builder factory with the loader
+	 * 
+	 * @param builderFactory
+	 *            the builder factory
+	 * @return the builder instance
+	 */
+	@NonNull
 	FXMLBuilder<N> builderFactory(BuilderFactory builderFactory);
+
+	/**
+	 * Load the FXML-Document
+	 * 
+	 * @return the root node
+	 * @throws IOException
+	 *             if there is a problem loading the fxml
+	 */
+	@NonNull
 	N load() throws IOException;
-	<C> Data loadWithController() throws IOException;
-	
-	interface Data<N,C> {
+
+	/**
+	 * Load the FXML and return the root node and the controller
+	 * 
+	 * @return a structure holding root node and controller
+	 * @throws IOException
+	 *             if there is a problem loading the fxml
+	 */
+	<C> Data<C, N> loadWithController() throws IOException;
+
+	/**
+	 * Data struct holing root node and controller
+	 * 
+	 * @param <N>
+	 *            the root node type
+	 * @param <C>
+	 *            the controller type
+	 */
+	public interface Data<N, C> {
+		/**
+		 * @return the root node
+		 */
+		@NonNull
 		public N getNode();
+
+		/**
+		 * @return the controller, might be <code>null</code> if the fxml is not
+		 *         associated to a controller
+		 */
 		public C getController();
 	}
 }
