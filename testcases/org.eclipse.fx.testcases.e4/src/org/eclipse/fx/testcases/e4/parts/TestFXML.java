b5d22e604d83b95f79e2e4459aa18fcf6531bd9e
Tom Schindl <tom.schindl@bestsolution.at>
2014-04-07 15:01:51 +0200

    Bug 432152 - E4: vertical toolbars don't work

--- testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/parts/TestFXML.java
+++ testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/parts/TestFXML.java
@@ -1,27 +1,22 @@
 package org.eclipse.fx.testcases.e4.parts;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
-import javafx.util.Callback;
 
 import org.eclipse.fx.core.fxml.FXMLDocument;
 
 public class TestFXML extends FXMLDocument<HBox> {
 
 	@Override
-	public HBox load(URL location, ResourceBundle bundle,
-			Callback<Class<?>, Object> controllerFactory) {
-		HBox b = new HBox();
-		b.getChildren().add(new Button("This is the Java class"));
-		return b;
+	public Object getController() {
+		return null;
 	}
 
 	@Override
-	public Object getController() {
-		return null;
+	public HBox load(org.eclipse.fx.core.fxml.FXMLDocument.LoadData<HBox> data) {
+		HBox b = new HBox();
+		b.getChildren().add(new Button("This is the Java class"));
+		return b;
 	}
 
 }
