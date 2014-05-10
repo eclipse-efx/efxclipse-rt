8a68e5e815ca5f34ca67943ee248d3a187c16bd7
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-10 14:51:37 +0200

    fixed javadoc

--- bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/services/PartPopupMenuServiceFunction.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/services/PartPopupMenuServiceFunction.java
@@ -13,9 +13,10 @@ package org.eclipse.fx.ui.workbench.renderers.fx.services;
 import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.services.EMenuService;
 
 /**
- * Context function to create a {@link PopupMenuService}
+ * Context function to create a {@link EMenuService}
  */
 public class PartPopupMenuServiceFunction extends ContextFunction {
 
