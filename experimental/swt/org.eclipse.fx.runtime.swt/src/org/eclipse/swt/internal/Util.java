9398c06d53d90fabcbec384f49f8fe72c548a807
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-26 09:49:49 +0200

    adding extra build for swt

--- experimental/swt/org.eclipse.fx.runtime.swt/src/org/eclipse/swt/internal/Util.java
+++ experimental/swt/org.eclipse.fx.runtime.swt/src/org/eclipse/swt/internal/Util.java
@@ -17,8 +17,8 @@ import javafx.collections.ObservableList;
 
 public class Util {
 	public static final void logNotImplemented() {
-		StackTraceElement e = Thread.currentThread().getStackTrace()[2];
-		System.err.println(e + " not implemented yet");
+//		StackTraceElement e = Thread.currentThread().getStackTrace()[2];
+//		System.err.println(e + " not implemented yet");
 	}
 	
 	public static final void removeListIndices(ObservableList<?> list, int[] indices) {
