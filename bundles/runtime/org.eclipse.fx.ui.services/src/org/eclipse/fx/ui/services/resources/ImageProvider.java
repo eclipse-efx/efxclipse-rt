fa117e89ce60d76b9a5ee505e6a2935796093308
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-26 23:55:27 +0200

    javadoc and null data

--- bundles/runtime/org.eclipse.fx.ui.services/src/org/eclipse/fx/ui/services/resources/ImageProvider.java
+++ bundles/runtime/org.eclipse.fx.ui.services/src/org/eclipse/fx/ui/services/resources/ImageProvider.java
@@ -14,11 +14,35 @@ import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jdt.annotation.NonNull;
 
 import javafx.scene.image.Image;
 
+/**
+ * An image provider
+ */
 public interface ImageProvider {
+	/**
+	 * @return name of the provider
+	 */
+	@NonNull
 	public String getName();
-	public List<String> getFileSuffix();
-	public Image getImage(URI uri) throws IOException;
+
+	/**
+	 * @return the supported file suffixes
+	 */
+	@NonNull
+	public List<@NonNull String> getFileSuffix();
+
+	/**
+	 * Loading the image from the given uri
+	 * 
+	 * @param uri
+	 *            the uri
+	 * @return the image
+	 * @throws IOException
+	 *             exception when loading fails
+	 */
+	@NonNull
+	public Image getImage(@NonNull URI uri) throws IOException;
 }
