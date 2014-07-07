28eb3d85bfcd98e970c582e150d5b2e51d69f5d5
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-07 14:25:24 +0200

    fixed javadoc and null informations

--- bundles/runtime/org.eclipse.fx.ui.panes/src/org/eclipse/fx/ui/panes/AbstractLayoutPane.java
+++ bundles/runtime/org.eclipse.fx.ui.panes/src/org/eclipse/fx/ui/panes/AbstractLayoutPane.java
@@ -10,69 +10,102 @@
  *******************************************************************************/
 package org.eclipse.fx.ui.panes;
 
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 
+/**
+ * Abstract layout pane
+ * 
+ * @param <D>
+ *            the constraint
+ */
 public abstract class AbstractLayoutPane<D> extends Pane {
+	/**
+	 * default
+	 */
 	public static final int FX_DEFAULT = -1;
-	public static final int FX_BEGINNING = 1;
-	public static final int FX_FILL = 4;
-	public static final int FX_LEFT = 16384;
-	public static final int FX_END = 16777224;
-	public static final int FX_RIGHT = 131072;
-	public static final int FX_CENTER = 16777216;
-	public static final int FX_BOTTOM = 1024;
-	public static final int FX_TOP = 128;
-	public static final int FX_HORIZONTAL = 256;
-	
+
 	static class Size {
 		public final double width;
 		public final double height;
-		
+
 		public Size(double width, double height) {
 			this.width = width;
 			this.height = height;
 		}
 	}
-	
-	protected abstract Size computeSize(double width, double height, boolean flushCache);
-	
-	protected static void setConstraint(Node node, Object key, Object value) {
-        if (value == null) {
-            node.getProperties().remove(key);
-        } else {
-            node.getProperties().put(key, value);
-        }
-        if (node.getParent() != null) {
-            node.getParent().requestLayout();
-        }
-    }
 
-	protected static Object getConstraint(Node node, Object key) {
-        if (node.hasProperties()) {
-            Object value = node.getProperties().get(key);
-            if (value != null) {
-                return value;
-            }
-        }
-        return null;
-    }
-	
+	/**
+	 * Compute the size
+	 * 
+	 * @param width
+	 *            the available width
+	 * @param height
+	 *            the available height
+	 * @param flushCache
+	 *            flush cached informations
+	 * @return the size
+	 */
+	protected abstract @NonNull Size computeSize(double width, double height, boolean flushCache);
+
+	/**
+	 * Remember a layout constraint
+	 * 
+	 * @param node
+	 *            the node
+	 * @param key
+	 *            the constraint key
+	 * @param value
+	 *            the value
+	 */
+	protected static void setConstraint(@NonNull Node node, @NonNull Object key, @Nullable Object value) {
+		if (value == null) {
+			node.getProperties().remove(key);
+		} else {
+			node.getProperties().put(key, value);
+		}
+		if (node.getParent() != null) {
+			node.getParent().requestLayout();
+		}
+	}
+
+	/**
+	 * Access the current constraint value
+	 * 
+	 * @param node
+	 *            the node
+	 * @param key
+	 *            the key
+	 * @return the value if associated
+	 */
+	protected static @Nullable Object getConstraint(@NonNull Node node, @NonNull Object key) {
+		if (node.hasProperties()) {
+			Object value = node.getProperties().get(key);
+			if (value != null) {
+				return value;
+			}
+		}
+		return null;
+	}
+
 	@Override
 	protected double computeMinHeight(double width) {
 		return 0;
 	}
-	
+
 	@Override
 	protected double computeMinWidth(double height) {
 		return 0;
 	}
-	
+
 	@Override
 	protected double computePrefHeight(double width) {
 		return computeSize(FX_DEFAULT, FX_DEFAULT, true).height;
 	}
-	
+
 	@Override
 	protected double computePrefWidth(double height) {
 		return computeSize(FX_DEFAULT, FX_DEFAULT, true).width;
