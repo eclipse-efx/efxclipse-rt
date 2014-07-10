4041ca2c24b57b4eff6368c39fad4e8d8632d769
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-10 23:00:46 +0200

    javadoc and null information

--- bundles/runtime/org.eclipse.fx.ui.keybindings.generic/src/org/eclipse/fx/ui/keybindings/generic/KeySequenceImpl.java
+++ bundles/runtime/org.eclipse.fx.ui.keybindings.generic/src/org/eclipse/fx/ui/keybindings/generic/KeySequenceImpl.java
@@ -19,7 +19,11 @@ import org.eclipse.fx.ui.keybindings.KeySequence;
 import org.eclipse.fx.ui.keybindings.KeyStroke;
 import org.eclipse.fx.ui.keybindings.ParseException;
 import org.eclipse.fx.ui.keybindings.TriggerSequence;
+import org.eclipse.jdt.annotation.NonNull;
 
+/**
+ * A keysequence implementation
+ */
 public class KeySequenceImpl extends TriggerSequenceImpl implements KeySequence {
 
 	@Override
@@ -27,19 +31,19 @@ public class KeySequenceImpl extends TriggerSequenceImpl implements KeySequence 
 		return Util.compare(getTriggers(), o.getTriggers());
 	}
 
-	private final static KeySequenceImpl EMPTY_KEY_SEQUENCE = new KeySequenceImpl(
-			new KeyStroke[0]);
-	
-	public final static String KEY_STROKE_DELIMITER = "\u0020"; //$NON-NLS-1$
-	public final static String KEY_STROKE_DELIMITERS = KEY_STROKE_DELIMITER
-			+ "\b\r\u007F\u001B\f\n\0\t\u000B"; //$NON-NLS-1$
-	
-	public KeySequenceImpl(KeyStroke[] triggers) {
+	@NonNull
+	private final static KeySequenceImpl EMPTY_KEY_SEQUENCE = new KeySequenceImpl(new KeyStroke[0]);
+
+	private final static String KEY_STROKE_DELIMITER = "\u0020"; //$NON-NLS-1$
+	private final static String KEY_STROKE_DELIMITERS = KEY_STROKE_DELIMITER + "\b\r\u007F\u001B\f\n\0\t\u000B"; //$NON-NLS-1$
+
+	KeySequenceImpl(KeyStroke[] triggers) {
 		super(triggers);
 	}
 
+	@SuppressWarnings("null")
 	@Override
-	public final TriggerSequence[] getPrefixes() {
+	public final @NonNull TriggerSequence[] getPrefixes() {
 		final int numberOfPrefixes = getTriggers().length;
 		final TriggerSequence[] prefixes = new TriggerSequence[numberOfPrefixes];
 		prefixes[0] = KeySequenceImpl.getInstance();
@@ -52,69 +56,52 @@ public class KeySequenceImpl extends TriggerSequenceImpl implements KeySequence 
 		return prefixes;
 	}
 
-
 	@Override
 	public String format() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	public static final KeySequence getInstance() {
+
+	static final @NonNull KeySequence getInstance() {
 		return EMPTY_KEY_SEQUENCE;
 	}
-	
-	public static final KeySequence getInstance(final KeyStroke[] keyStrokes) {
+
+	static final @NonNull KeySequence getInstance(final @NonNull KeyStroke[] keyStrokes) {
 		return new KeySequenceImpl(keyStrokes);
 	}
-	
-	public static final KeySequence getInstance(final KeySequence keySequence,
-			final KeyStroke keyStroke) {
-		if (keySequence == null || keyStroke == null) {
-			throw new NullPointerException();
-		}
 
+	static final @NonNull KeySequence getInstance(final @NonNull KeySequence keySequence, final @NonNull KeyStroke keyStroke) {
 		final KeyStroke[] oldKeyStrokes = keySequence.getKeyStrokes();
 		final int oldKeyStrokeLength = oldKeyStrokes.length;
 		final KeyStroke[] newKeyStrokes = new KeyStroke[oldKeyStrokeLength + 1];
-		System
-				.arraycopy(oldKeyStrokes, 0, newKeyStrokes, 0,
-						oldKeyStrokeLength);
+		System.arraycopy(oldKeyStrokes, 0, newKeyStrokes, 0, oldKeyStrokeLength);
 		newKeyStrokes[oldKeyStrokeLength] = keyStroke;
 		return new KeySequenceImpl(newKeyStrokes);
 	}
 
-	public static final KeySequence getInstance(KeyLookup lookup, final String string)
-			throws ParseException {
-		if (string == null) {
-			throw new NullPointerException();
-		}
-
+	static final @NonNull KeySequence getInstance(@NonNull KeyLookup lookup, @NonNull final String string) throws ParseException {
 		final List<KeyStroke> keyStrokes = new ArrayList<KeyStroke>();
-		final StringTokenizer stringTokenizer = new StringTokenizer(string,
-				KEY_STROKE_DELIMITERS);
+		final StringTokenizer stringTokenizer = new StringTokenizer(string, KEY_STROKE_DELIMITERS);
 
 		try {
 			while (stringTokenizer.hasMoreTokens()) {
-				keyStrokes.add(KeyStrokeImpl.getInstance(lookup, stringTokenizer
-						.nextToken()));
+				keyStrokes.add(KeyStrokeImpl.getInstance(lookup, stringTokenizer.nextToken()));
 			}
 
-			final KeyStroke[] keyStrokeArray = (KeyStroke[]) keyStrokes
-					.toArray(new KeyStroke[keyStrokes.size()]);
+			final KeyStroke[] keyStrokeArray = (KeyStroke[]) keyStrokes.toArray(new KeyStroke[keyStrokes.size()]);
 			return new KeySequenceImpl(keyStrokeArray);
 		} catch (final IllegalArgumentException e) {
-			throw new ParseException(
-					"Could not construct key sequence with these key strokes: " //$NON-NLS-1$
-							+ keyStrokes);
+			throw new ParseException("Could not construct key sequence with these key strokes: " //$NON-NLS-1$
+					+ keyStrokes);
 		} catch (final NullPointerException e) {
-			throw new ParseException(
-					"Could not construct key sequence with these key strokes: " //$NON-NLS-1$
-							+ keyStrokes);
+			throw new ParseException("Could not construct key sequence with these key strokes: " //$NON-NLS-1$
+					+ keyStrokes);
 		}
 	}
 
+	@SuppressWarnings("null")
 	@Override
-	public final KeyStroke[] getKeyStrokes() {
+	public final @NonNull KeyStroke[] getKeyStrokes() {
 		final int triggerLength = getTriggers().length;
 		final KeyStroke[] keyStrokes = new KeyStroke[triggerLength];
 		System.arraycopy(getTriggers(), 0, keyStrokes, 0, triggerLength);
