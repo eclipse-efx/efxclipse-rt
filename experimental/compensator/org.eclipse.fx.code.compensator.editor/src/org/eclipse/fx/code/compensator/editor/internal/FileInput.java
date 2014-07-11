2721af531adc3638f0f76fdb059a3fb46cfd9c99
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-12 01:17:59 +0200

    added new groovy content type

--- experimental/compensator/org.eclipse.fx.code.compensator.editor/src/org/eclipse/fx/code/compensator/editor/internal/FileInput.java
+++ experimental/compensator/org.eclipse.fx.code.compensator.editor/src/org/eclipse/fx/code/compensator/editor/internal/FileInput.java
@@ -57,13 +57,14 @@ public class FileInput implements Input<String>, ContentTypeProvider {
 	@Override
 	public String getContentType() {
 		//FIXME Need suffix => type mapping
-		System.err.println("PATH: " + path);
 		if( path.toString().endsWith(".xml") ) {
 			return ContentTypeProvider.XML;
 		} else if( path.toString().endsWith(".java") ) {
 			return ContentTypeProvider.JAVA;
 		} else if( path.toString().endsWith(".js") ) {
 			return ContentTypeProvider.JAVASCRIPT;
+		} else if( path.toString().endsWith(".groovy") ) {
+			return ContentTypeProvider.GROOVY;
 		}
 		return null;
 	}
