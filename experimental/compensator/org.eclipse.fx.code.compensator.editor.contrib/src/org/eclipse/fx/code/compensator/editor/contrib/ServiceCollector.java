fcd4a1697fd2a76dd2d6998c6e70fe215f607547
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-12 01:17:30 +0200

    remove debug info

--- experimental/compensator/org.eclipse.fx.code.compensator.editor.contrib/src/org/eclipse/fx/code/compensator/editor/contrib/ServiceCollector.java
+++ experimental/compensator/org.eclipse.fx.code.compensator.editor.contrib/src/org/eclipse/fx/code/compensator/editor/contrib/ServiceCollector.java
@@ -54,7 +54,6 @@ public class ServiceCollector {
 	}
 	
 	public void addSourceViewerConfigurationFactory(SourceViewerConfigurationFactory provider) {
-		System.err.println("REGISTERING source provider: " + provider);
 		configurationProvider.add(provider);
 	}
 	
