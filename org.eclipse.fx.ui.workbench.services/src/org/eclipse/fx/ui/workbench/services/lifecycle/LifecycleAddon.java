36ca1e7fff5e3dae3a21c7b0724564109617a7fa
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-26 00:06:43 +0200

    Bug 440434 - Tag modifications do not update widget styleclasses

---
+++ org.eclipse.fx.ui.workbench.services/src/org/eclipse/fx/ui/workbench/services/lifecycle/LifecycleAddon.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ui.workbench.services.lifecycle;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.fx.ui.workbench.services.ELifecycleService;
+
+//when we are ready to ditch the tag-based stuff we simply swap the addon
+/**
+ * Addon to add so that lifecycle stuff is initialized
+ */
+public class LifecycleAddon {
+
+	@Inject
+	MApplication app;
+
+	@Inject
+	ELifecycleService lifecycleService;
+
+	/**
+	 * Key used to store the lifecycle instances inside the transient map
+	 */
+	public static final String LIFECYCLE_TRANSIENT_KEY = "EFX_LIFECYCLE"; //$NON-NLS-1$
+	
+	/**
+	 * Tag prefix to use
+	 */
+	public static final String LIFECYCLE_TAG_PREFIX = "EFX_LC:"; //$NON-NLS-1$
+
+	@SuppressWarnings("null")
+	@PostConstruct
+	void postConstruct() {
+		TreeIterator<EObject> it = EcoreUtil.getAllContents((EObject) this.app, true);
+		while (it.hasNext()) {
+			EObject e = it.next();
+			if (e instanceof MUIElement) {
+				MUIElement element = (MUIElement) e;
+				List<String> tags = element.getTags();
+				for (String tag : tags) {
+					if (tag.startsWith(LIFECYCLE_TAG_PREFIX)) {
+						this.lifecycleService.registerLifecycleURI(element, tag.substring(LIFECYCLE_TAG_PREFIX.length()));
+					}
+				}
+			}
+		}
+	}
+}
---
+++ org.eclipse.fx.ui.workbench.services/src/org/eclipse/fx/ui/workbench/services/lifecycle/annotation/PreClose.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ui.workbench.services.lifecycle.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Called before something like Part, Perspective, Window, ... is closed
+ * allowing to veto it
+ */
+@Documented
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PreClose {
+	// nothing
+}
