2ef5bf4c303f3bb2dd05399f294f2c2edeae7fb5
Tom Schindl <tom.schindl@bestsolution.at>
2014-08-27 18:37:02 +0200

    added menu to reproduce bug

--- demos/org.eclipse.fx.testcases.dnd.app/src/org/eclipse/fx/testcases/dnd/app/PartImpl.java
+++ demos/org.eclipse.fx.testcases.dnd.app/src/org/eclipse/fx/testcases/dnd/app/PartImpl.java
@@ -1,13 +1,14 @@
 package org.eclipse.fx.testcases.dnd.app;
 
+import javafx.collections.FXCollections;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 
 public class PartImpl {
@@ -15,17 +16,20 @@ public class PartImpl {
 
 	@PostConstruct
 	void init(BorderPane parent, MPart part) {
-		Label l = new Label(part.getLabel());
-		l.setFont(Font.font(30));
-		
+//		Label l = new Label(part.getLabel());
+//		l.setFont(Font.font(30));
+//		
+//		parent.setCenter(l);
+//		
+//		f = new TextField();
+//		parent.setBottom(f);
+		ListView<String> l = new ListView<String>();
+		l.setItems(FXCollections.observableArrayList("A","B","C"));
 		parent.setCenter(l);
-		
-		f = new TextField();
-		parent.setBottom(f);
 	}
 	
-	@Focus
-	void focus() {
-		f.requestFocus();
-	}
+//	@Focus
+//	void focus() {
+//		f.requestFocus();
+//	}
 }
