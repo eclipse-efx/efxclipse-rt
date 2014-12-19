e4b3999256953bd215eaddf8799d579d8843976f
Tom Schindl <tom.schindl@bestsolution.at>
2014-12-19 13:58:45 +0100

    Bug 455738 - A control who can display a StyledString

---
+++ bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/styledtext/skin/StyledLabelSkin.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * 	Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ui.controls.styledtext.skin;
+
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import org.eclipse.fx.ui.controls.Util;
+import org.eclipse.fx.ui.controls.styledtext.StyledLabel;
+import org.eclipse.fx.ui.controls.styledtext.StyledString;
+import org.eclipse.fx.ui.controls.styledtext.behavior.StyledLabelBehavior;
+
+import com.sun.javafx.scene.control.skin.BehaviorSkinBase;
+
+/**
+ * Skin for {@link StyledLabel}
+ */
+@SuppressWarnings("restriction")
+public class StyledLabelSkin extends
+		BehaviorSkinBase<StyledLabel, StyledLabelBehavior> {
+	private final Pane b;
+	private final Label l;
+
+	/**
+	 * Create a new skin
+	 *
+	 * @param control
+	 *            the control
+	 */
+	public StyledLabelSkin(StyledLabel control) {
+		super(control, new StyledLabelBehavior(control));
+		this.b = new HBox(10);
+		this.l = new Label();
+		this.l.getStyleClass().clear();
+		Bindings.bindContent(this.l.getStyleClass(), control.getStyleClass());
+		this.b.getChildren().add(this.l);
+
+		control.styledStringProperty().addListener(this::styleStringChanged);
+		getChildren().add(this.b);
+		rebuildControl();
+	}
+
+	private void styleStringChanged(
+			ObservableValue<? extends StyledString> observable,
+			StyledString oldValue, StyledString newValue) {
+		if (oldValue != null) {
+			oldValue.getSegmentList().removeListener(this::segmentsChanged);
+		}
+		if (newValue != null) {
+			newValue.getSegmentList().addListener(this::segmentsChanged);
+		}
+	}
+
+	private void segmentsChanged(Observable o) {
+		rebuildControl();
+	}
+
+	private void rebuildControl() {
+		if( this.b.getChildren().size() > 1 ) {
+			this.b.getChildren().set(1,
+					Util.toNode(getSkinnable().getStyleString()));
+		} else {
+			this.b.getChildren().add(
+					Util.toNode(getSkinnable().getStyleString()));
+		}
+
+	}
+
+	@Override
+	public void dispose() {
+		getSkinnable().getStyleString().getSegmentList()
+				.removeListener(this::segmentsChanged);
+		getSkinnable().styledStringProperty().removeListener(
+				this::styleStringChanged);
+		Bindings.unbindContent(this.l.getStyleClass(), getSkinnable().getStyleClass());
+		super.dispose();
+	}
+
+}
