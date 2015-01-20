40ac3d2fbe44b581070221d9f5b19ddc119b66fb
Tom Schindl <tom.schindl@bestsolution.at>
2015-01-20 09:53:34 +0100

    use new graphics service

--- experimental/compensator/org.eclipse.fx.code.compensator.project.jdt/src/org/eclipse/fx/code/compensator/project/jdt/internal/JDTProposalComputerFactory.java
+++ experimental/compensator/org.eclipse.fx.code.compensator.project.jdt/src/org/eclipse/fx/code/compensator/project/jdt/internal/JDTProposalComputerFactory.java
@@ -6,28 +6,50 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import javafx.scene.Node;
+
+import org.eclipse.fx.code.compensator.editor.Input;
 import org.eclipse.fx.code.compensator.editor.ProposalComputer;
 import org.eclipse.fx.code.compensator.editor.ProposalComputer.ProposalContext;
-import org.eclipse.fx.code.compensator.editor.Input;
 import org.eclipse.fx.code.compensator.editor.services.ProposalComputerFactory;
 import org.eclipse.fx.code.server.jdt.shared.JavaCodeCompleteProposal;
+import org.eclipse.fx.code.server.jdt.shared.JavaCodeCompleteProposal.Modifier;
 import org.eclipse.fx.code.server.jdt.shared.JavaCodeCompleteProposal.Type;
-import org.eclipse.fx.code.server.jdt.shared.JavaCodeCompleteProposal.Visibility;
+import org.eclipse.fx.core.URI;
+import org.eclipse.fx.core.log.Logger;
+import org.eclipse.fx.core.log.Logger.Level;
+import org.eclipse.fx.core.log.LoggerCreator;
 import org.eclipse.fx.ui.controls.styledtext.StyledString;
+import org.eclipse.fx.ui.services.resources.AdornedImageDescriptor;
+import org.eclipse.fx.ui.services.resources.AdornedImageDescriptor.Adornment;
+import org.eclipse.fx.ui.services.resources.AdornedImageDescriptor.Location;
+import org.eclipse.fx.ui.services.resources.GraphicsLoader;
 import org.eclipse.jface.text.contentassist.CompletetionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
 public class JDTProposalComputerFactory implements ProposalComputerFactory {
+	private static Logger LOGGER;
+
+	private static Logger getLogger() {
+		if( LOGGER == null ) {
+			LOGGER = LoggerCreator.createLogger(JDTProposalComputerFactory.class);
+		}
+		return LOGGER;
+	}
 
 	@Override
 	public boolean applies(Input<?> input) {
+		if( getLogger().isEnabled(Level.DEBUG) ) {
+			getLogger().debug("Checking of the computer applies to '"+input+"'");
+		}
 		return input instanceof JDTServerInput;
 	}
 
 	@Override
-	public ProposalComputer createProposalComputer() {
+	public ProposalComputer createProposalComputer(GraphicsLoader graphicsLoader) {
 		return new ProposalComputer() {
 
 			@Override
@@ -35,10 +57,9 @@ public class JDTProposalComputerFactory implements ProposalComputerFactory {
 				try {
 					JDTServerInput i = (JDTServerInput) context.input;
 					Future<List<org.eclipse.fx.code.server.jdt.shared.Proposal>> proposals = i.server.getProposals(i.openFile.get(), context.location);
-					System.err.println("GOING TO TRANSFORM: " + proposals.get());
 					return CompletableFuture.supplyAsync(() -> {
 						try {
-							return proposals.get().stream().map((p) -> createProposal(context,p)).collect(Collectors.toList());
+							return proposals.get().stream().map((p) -> createProposal(context,p,graphicsLoader)).collect(Collectors.toList());
 						} catch (Exception e) {
 							throw new RuntimeException(e);
 						}
@@ -52,7 +73,7 @@ public class JDTProposalComputerFactory implements ProposalComputerFactory {
 		};
 	}
 
-	static ICompletionProposal createProposal(ProposalContext context, org.eclipse.fx.code.server.jdt.shared.Proposal proposal) {
+	static ICompletionProposal createProposal(ProposalContext context, org.eclipse.fx.code.server.jdt.shared.Proposal proposal, GraphicsLoader graphicsLoader) {
 		JavaCodeCompleteProposal cp = (JavaCodeCompleteProposal) proposal;
 		StyledString s = new StyledString();
 		s.appendSegment(cp.getLabel(), "java-element-name");
@@ -60,29 +81,83 @@ public class JDTProposalComputerFactory implements ProposalComputerFactory {
 			s.appendSegment(cp.getAdditionalLabel(), "java-type-info");
 		}
 
-		List<String> styles = new ArrayList<>();
+		URI baseImage;
+		List<Adornment> adornments = new ArrayList<>();
 
 		if( cp.getType() == Type.CLASS ) {
-			styles.add("java-class-"+visibilityAddon(cp.getVisibility()));
+			baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/classpub_obj.png");
+
+			if( isStatic(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/static_co.png")));
+			} else if( isFinal(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/final_co.png")));
+			}
 		} else if( cp.getType() == Type.METHOD ) {
-			styles.add("java-method-"+visibilityAddon(cp.getVisibility()));
+			switch (cp.getVisibility()) {
+			case PACKAGE_PRIVATE:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/methdef_obj.png");
+				break;
+			case PRIVATE:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/methpri_obj.png");
+				break;
+			case PROTECTED:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/methpro_obj.png");
+				break;
+			default:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/methpub_obj.png");
+				break;
+			}
+
+			if( isStatic(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/static_co.png")));
+			} else if( isFinal(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/final_co.png")));
+			}
 		} else if( cp.getType() == Type.FIELD ) {
-			styles.add("java-field-"+visibilityAddon(cp.getVisibility()));
+			switch (cp.getVisibility()) {
+			case PACKAGE_PRIVATE:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/field_default_obj.png");
+				break;
+			case PRIVATE:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/field_private_obj.png");
+				break;
+			case PROTECTED:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/field_protected_obj.png");
+				break;
+			default:
+				baseImage = URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/16/field_public_obj.png");
+				break;
+			}
+
+			if( isStatic(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/static_co.png")));
+			} else if( isFinal(cp.getModifiers()) ) {
+				adornments.add(new Adornment(Location.RIGHT_TOP,URI.createPlatformPluginURI("org.eclipse.fx.code.compensator.jdt", "css/icons/7/final_co.png")));
+			}
+
+		} else {
+			baseImage = null;
 		}
 
-		return new CompletetionProposal(cp.getValue(), cp.getReplacementOffset(), cp.getReplacementLength(),s, () -> null);
+		Supplier<Node> supplier;
+		if( baseImage != null ) {
+			if( adornments.isEmpty() ) {
+				supplier = () -> graphicsLoader.getGraphicsNode(baseImage);
+			} else {
+				supplier = () -> graphicsLoader.getGraphicsNode(new AdornedImageDescriptor(baseImage, adornments));
+			}
+		} else {
+			supplier = () -> null;
+		}
+
+		return new CompletetionProposal(cp.getValue(), cp.getReplacementOffset(), cp.getReplacementLength(),s, supplier);
 	}
 
-	private static String visibilityAddon(Visibility v) {
-		switch (v) {
-		case PACKAGE_PRIVATE:
-			return "default";
-		case PRIVATE:
-			return "private";
-		case PROTECTED:
-			return "protected";
-		default:
-			return "public";
-		}
+	private static final boolean isFinal(List<Modifier> m) {
+		return m.contains(Modifier.FINAL);
+	}
+
+	private static final boolean isStatic(List<Modifier> m) {
+		return m.contains(Modifier.STATIC);
 	}
 }
