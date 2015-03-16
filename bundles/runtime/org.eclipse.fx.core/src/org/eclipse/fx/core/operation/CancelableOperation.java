50572ea883438389ccb834facc966b15b9f0ddc3
Tom Schindl <tom.schindl@bestsolution.at>
2015-03-17 00:27:31 +0100

    Bug 462293 - Add operation abstraction for long running tasks

---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/operation/CancelableOperation.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.operation;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Abstract operation who can be canceled
+ * 
+ * @param <T>
+ *            the type produced when completed successfully
+ */
+public interface CancelableOperation<T> extends Operation<T> {
+	/**
+	 * Called when the operation is canceled
+	 * 
+	 * @param consumer
+	 *            the consumer
+	 */
+	public void onCancel(@NonNull Runnable consumer);
+	
+	/**
+	 * Cancel the running operation
+	 */
+	public void cancel();
+}
---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/operation/Operation.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.operation;
+
+import java.util.function.Consumer;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Abstract operation who is running async and informs about its state
+ * 
+ * @param <T>
+ *            the value produced by the operation
+ */
+public interface Operation<T> {
+	/**
+	 * Called when the operation completes successfully
+	 * 
+	 * @param consumer
+	 *            consumer invoked
+	 */
+	public void onComplete(@NonNull Consumer<@Nullable T> consumer);
+
+	/**
+	 * Called when the operation completes with an exception
+	 * 
+	 * @param consumer
+	 *            the consumer invoked
+	 */
+	public void onException(@NonNull Consumer<@NonNull Throwable> consumer);
+}
