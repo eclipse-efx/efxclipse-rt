7c1fb7efacea571ee26d59edb6da68c52b98f0f2
Christoph Keimel <c.keimel@emsw.de>
2015-03-25 14:33:30 +0100

    Bug 463182 - [swtonjavafx] Add ContextFunction to inject Display
    Signed-off-by: Christoph Keimel <c.keimel@emsw.de>
    Change-Id: I7bb18bee696fb2d5c867230a97343798c3c3cbde

---
+++ experimental/swt/org.eclipse.fx.runtime.swt.e4/src/org/eclipse/fx/runtime/swt/e4/FXToSWTDisplayFunction.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2015 EM-SOFTWARE and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Keimel <c.keimel@emsw.de> - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.fx.runtime.swt.e4;
+
+import org.eclipse.e4.core.contexts.ContextFunction;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.swt.widgets.Display;
+
+public class FXToSWTDisplayFunction extends ContextFunction {
+	@Override
+	public Object compute(IEclipseContext context) {
+		return Display.getCurrent();
+	}
+}
