8180c1bec0833bae2ebb1ddd6cacb2311320ff51
Tom Schindl <tom.schindl@bestsolution.at>
2015-04-22 21:46:05 +0200

    Bug 464382 - Add support to show lightweight dialogs

--- testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/handlers/OpenLightweightWindow.java
+++ testcases/org.eclipse.fx.testcases.e4/src/org/eclipse/fx/testcases/e4/handlers/OpenLightweightWindow.java
@@ -1,13 +1,19 @@
 package org.eclipse.fx.testcases.e4.handlers;
 
+import javax.inject.Named;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.fx.testcases.e4.lightweight.SimpleWindow;
 import org.eclipse.fx.ui.services.dialog.LightWeightDialogService;
 import org.eclipse.fx.ui.services.dialog.LightWeightDialogService.ModalityScope;
 
 public class OpenLightweightWindow {
 	@Execute
-	public void run(LightWeightDialogService service) {
-		service.openDialog(SimpleWindow.class, ModalityScope.WINDOW);
+	public void run(IEclipseContext context, MPart p, LightWeightDialogService service, @Named("scope") String scope) {
+		System.err.println("CONTEXT: " + context);
+		System.err.println(p);
+		service.openDialog(SimpleWindow.class, ModalityScope.valueOf(scope));
 	}
 }
