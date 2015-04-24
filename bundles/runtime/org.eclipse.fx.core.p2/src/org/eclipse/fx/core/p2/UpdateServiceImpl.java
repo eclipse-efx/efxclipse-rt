b37dd10eef038c87b8972b1dd621f4d9388a7220
Tom Schindl <tom.schindl@bestsolution.at>
2015-04-24 10:18:12 +0200

    Bug 465375 - UpdateSerice not resolved if p2.core is not already started

--- bundles/runtime/org.eclipse.fx.core.p2/src/org/eclipse/fx/core/p2/UpdateServiceImpl.java
+++ bundles/runtime/org.eclipse.fx.core.p2/src/org/eclipse/fx/core/p2/UpdateServiceImpl.java
@@ -32,10 +32,13 @@ import org.eclipse.fx.core.log.LoggerFactory;
 import org.eclipse.fx.core.operation.CancelableOperation;
 import org.eclipse.fx.core.update.UpdateService;
 import org.eclipse.jdt.annotation.NonNull;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * Implementation of service based upon p2
  */
+@Component
 public class UpdateServiceImpl implements UpdateService {
 	private Logger logger;
 	private LoggerFactory factory;
@@ -82,6 +85,7 @@ public class UpdateServiceImpl implements UpdateService {
 	 * @param factory
 	 *            the factory
 	 */
+	@Reference
 	public void setLoggerFactory(LoggerFactory factory) {
 		this.factory = factory;
 		if (this.logger != null) {
@@ -106,7 +110,10 @@ public class UpdateServiceImpl implements UpdateService {
 	 * Set the provisioning agent
 	 * @param agent the provisioning agent
 	 */
+	@Reference
 	public void setProvisioningAgent(IProvisioningAgent agent) {
+		// We need to keep the relation 0..1 because IProvisioningAgent is only loaded
+		// if the p2.core bundle is started!
 		this.provisioningAgent = agent;
 	}
 	
@@ -173,155 +180,4 @@ public class UpdateServiceImpl implements UpdateService {
 		o.schedule();
 		return op;
 	}
-
-	// TODO enable forced update support in future
-	// protected IQueryable<IInstallableUnit> collectUpdateableUnits(
-	// IProvisioningAgent agent, final IProgressMonitor monitor)
-	// throws ProvisionException {
-	// IRepositoryManager<?> manager = (IRepositoryManager<?>) agent
-	// .getService(IMetadataRepositoryManager.class.getName());
-	//
-	// // Get update site uri's from p2.inf.
-	//
-	// final StringBuffer siteUrlPackage = new StringBuffer();
-	//
-	// URI[] uris = manager
-	// .getKnownRepositories(IRepositoryManager.REPOSITORIES_ALL);
-	//
-	// if (uris.length == 0) {
-	// getLogger().warning("no update repositories found");
-	// } else {
-	// for (URI uri : uris) {
-	// if (siteUrlPackage.length() > 0) {
-	// siteUrlPackage.append(",");
-	// }
-	// // LOGGER.info( "uri found: " + uri.toString() );
-	// siteUrlPackage.append(uri.toString());
-	// }
-	// }
-	//
-	// if (siteUrlPackage == null || siteUrlPackage.toString().isEmpty()) {
-	// throw new IllegalStateException(
-	// "No site URL specified. Edit your p2.inf file.");
-	// }
-	//
-	// getLogger().info( "Synch repos: " + siteUrlPackage );
-	//
-	// String[] siteUrlStrings = siteUrlPackage.toString().split(",");
-	// URI[] siteURIs = new URI[siteUrlStrings.length];
-	// for (int i = 0; i < siteURIs.length; i++) {
-	// try {
-	// siteURIs[i] = new URI(siteUrlStrings[i]);
-	// } catch (URISyntaxException e) {
-	// throw new IllegalStateException(e);
-	// }
-	// }
-	//
-	// // Convert URIs into something we can query...
-	// IQueryable<IInstallableUnit> allTheIUs = createIUQueryable(siteURIs,
-	// agent, monitor);
-	// getLogger().debug("query result: " + allTheIUs);
-	//
-	// return allTheIUs;
-	//
-	// }
-	//
-	// public void installUnits(final IProvisioningAgent agent,
-	// IQueryable<IInstallableUnit> allTheIUs, IProgressMonitor monitor) {
-	// ProvisioningSession session = new ProvisioningSession(agent);
-	// /*
-	// * Query the metadata repository(ies) for the feature(s) to install.
-	// */
-	// Collection<IInstallableUnit> toInstall = allTheIUs.query(
-	// QueryUtil.createIUGroupQuery(), new NullProgressMonitor())
-	// .toUnmodifiableSet();
-	// getLogger().info("Everything to synchronize:");
-	// logQueryResults(toInstall);
-	//
-	// SynchronizeOperation operation = new SynchronizeOperation(session,
-	// toInstall);
-	// IStatus opStatus = operation.resolveModal(monitor);
-	// if (opStatus.isOK()) {
-	// ProvisioningJob job = operation.getProvisioningJob(null);
-	// opStatus = job.runModal(monitor);
-	// if (opStatus.getSeverity() == IStatus.CANCEL)
-	// throw new OperationCanceledException();
-	// }
-	// }
-	//
-	// private void logQueryResults(Collection<IInstallableUnit> toInstall) {
-	// if (getLogger().isEnabled(Level.DEBUG)) {
-	// StringBuffer iuNames = new StringBuffer("IUs:\n\n ");
-	// for (IInstallableUnit iu : toInstall) {
-	// iuNames.append(iu.getId() + iu.getVersion() + "\n ");
-	// }
-	// getLogger().debug(iuNames.toString());
-	// }
-	// }
-	//
-	// private IQueryable<IInstallableUnit> createIUQueryable(URI[] p2Sites,
-	// IProvisioningAgent agent, IProgressMonitor monitor)
-	// throws ProvisionException {
-	// IMetadataRepositoryManager metadataManager = (IMetadataRepositoryManager) agent
-	// .getService(IMetadataRepositoryManager.SERVICE_NAME);
-	// IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent
-	// .getService(IArtifactRepositoryManager.SERVICE_NAME);
-	//
-	// final Collection<IMetadataRepository> metadataReposList = new LinkedList<IMetadataRepository>();
-	// for (URI uri : p2Sites) {
-	// getLogger().debug("Adding metadata repo at: " + uri.toString());
-	// try {
-	// IMetadataRepository metadataRepo = metadataManager
-	// .loadRepository(uri, monitor);
-	// getLogger().debug(
-	// "metadata repository loaded: " + uri.toString());
-	// try {
-	// artifactManager.loadRepository(uri, monitor); // load the
-	// // repo:
-	// // this
-	// // makes it
-	// // available
-	// // to us
-	// metadataReposList.add(metadataRepo);
-	// } catch (NullPointerException e) {
-	// getLogger()
-	// .warning(
-	// "Repository " + uri
-	// + " not found, will be ignored", e);
-	// }
-	// } catch (ProvisionException e) {
-	// if (e.getStatus().getCode() == ProvisionException.REPOSITORY_NOT_FOUND) {
-	// getLogger()
-	// .warning(
-	// "Repository " + uri
-	// + " not found, will be ignored");
-	// } else {
-	// getLogger()
-	// .warning(
-	// "ProvisionException code="
-	// + e.getStatus().getCode()
-	// + ", return none");
-	// throw e;
-	// }
-	// } catch (OperationCanceledException e) {
-	// getLogger().warning("OperationCanceledException, return none");
-	// throw e;
-	// }
-	// }
-	// if (metadataReposList.isEmpty()) {
-	// getLogger().debug("seems to be correct, return NONE");
-	// return null;
-	// } else {
-	// getLogger().debug("seems to be correct, return SOME");
-	// return QueryUtil.compoundQueryable(metadataReposList);
-	// }
-	// }
-	// private Collection<IInstallableUnit> getInstalledIUs() {
-	// IProfile profile = session.getProfileRegistry().getProfile(profileId);
-	// if (profile == null)
-	// return CollectionUtils.emptyList();
-	// IQuery<IInstallableUnit> query = new UserVisibleRootQuery();
-	// IQueryResult<IInstallableUnit> queryResult = profile.query(query, null);
-	// return queryResult.toUnmodifiableSet();
-	// }
 }
