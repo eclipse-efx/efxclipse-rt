a7b741d8f2f4a89c5b8a6339a12576f3e35e5ca9
Tom Schindl <tom.schindl@bestsolution.at>
2015-06-15 21:17:27 +0200

    Bug 470216 - MToolControl needs to be accessible when the creating the instance of the contributionURI

--- bundles/runtime/org.eclipse.fx.ui.workbench.renderers.base/src/org/eclipse/fx/ui/workbench/renderers/base/Util.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.renderers.base/src/org/eclipse/fx/ui/workbench/renderers/base/Util.java
@@ -1,15 +1,29 @@
 package org.eclipse.fx.ui.workbench.renderers.base;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.internal.contexts.Computation;
+import org.eclipse.e4.core.internal.contexts.ContextChangeEvent;
+import org.eclipse.e4.core.internal.contexts.EclipseContext;
+import org.eclipse.e4.core.internal.contexts.ValueComputation;
+import org.eclipse.e4.core.internal.contexts.WeakGroupedListenerList;
+import org.eclipse.fx.core.log.LoggerCreator;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.osgi.service.event.Event;
 
 /**
  * Collection of utility methods
  */
+@SuppressWarnings("restriction")
 public class Util {
 
 	/**
@@ -22,7 +36,7 @@ public class Util {
 	 * @return collection of elements
 	 */
 	@SuppressWarnings({ "unchecked", "null" })
-	@NonNull 
+	@NonNull
 	public static <E> Collection<@NonNull E> asCollection(Event event, String propertyName) {
 		Object o = event.getProperty(propertyName);
 		return o instanceof Collection<?> ? (Collection<E>) o : Collections.singleton((E) o);
@@ -40,18 +54,88 @@ public class Util {
 	public static String toCSSId(@NonNull String id) {
 		return id.replace('.', '-');
 	}
-	
-	public static IEclipseContext createLocalHandlerContext(IEclipseContext parent, IEclipseContext activeLeaf, String name) {
+
+	/**
+	 * Create a local handler
+	 * 
+	 * @param parent
+	 *            the parent context
+	 * @param activeLeaf
+	 *            the active leaf
+	 * @param name
+	 *            the name of the context
+	 * @return a new context
+	 */
+	public static IEclipseContext createLocalHandlerContext(@NonNull IEclipseContext parent, @NonNull IEclipseContext activeLeaf, @NonNull String name) {
 		IEclipseContext rv = parent.createChild(name);
 		rv.set("efx_handler_context", activeLeaf); //$NON-NLS-1$
 		return rv;
 	}
-	
-	public static IEclipseContext getActiveHandlerContext(IEclipseContext context) {
-		return (IEclipseContext) context.get("efx_handler_context");
+
+	/**
+	 * Extract the active leaf from the current context
+	 * 
+	 * @param context
+	 *            the context
+	 * @return the active leaf or <code>null</code>
+	 */
+	public static @Nullable IEclipseContext getActiveHandlerContext(@NonNull IEclipseContext context) {
+		return (IEclipseContext) context.get("efx_handler_context"); //$NON-NLS-1$
 	}
-	
-	public static boolean isLocalHandlerContext(IEclipseContext context) {
+
+	/**
+	 * Check if the context is handler context
+	 * 
+	 * @param context
+	 *            the context
+	 * @return <code>true</code> if the context is local handler context (or a
+	 *         child of such one)
+	 */
+	public static boolean isLocalHandlerContext(@NonNull IEclipseContext context) {
 		return context.get("efx_handler_context") != null; //$NON-NLS-1$
+	}
+
+	/**
+	 * Reparent a context
+	 * 
+	 * @param target
+	 *            the target
+	 * @param newParent
+	 *            the new parent
+	 */
+	public static void setParentContext(@NonNull IEclipseContext target, @NonNull IEclipseContext newParent) {
+		target.setParent(newParent);
+		// FIXME Remove this once bug 468048 has been fixed
+		EclipseContext c = (EclipseContext) target;
+		try {
+			invalidateLocalComputations(c, new HashSet<>());
+		} catch (Throwable t) {
+			LoggerCreator.createLogger(Util.class).error("Failed to invalidate context", t); //$NON-NLS-1$
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void invalidateLocalComputations(EclipseContext c, @SuppressWarnings("rawtypes") Set scheduled) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException {
+		Field localValueComputationsField = c.getClass().getDeclaredField("localValueComputations"); //$NON-NLS-1$
+		localValueComputationsField.setAccessible(true);
+		Map<String, ValueComputation> localValueComputations = (Map<String, ValueComputation>) localValueComputationsField.get(c);
+
+		Field weakListenersField = c.getClass().getDeclaredField("weakListeners"); //$NON-NLS-1$
+		weakListenersField.setAccessible(true);
+		WeakGroupedListenerList weakListeners = (WeakGroupedListenerList) weakListenersField.get(c);
+
+		Constructor<?> constructor = ContextChangeEvent.class.getDeclaredConstructors()[0];
+		constructor.setAccessible(true);
+		ContextChangeEvent event = (ContextChangeEvent) constructor.newInstance(c, Integer.valueOf(ContextChangeEvent.ADDED), null, null, null);
+		for (Computation computation : localValueComputations.values()) {
+			weakListeners.remove(computation);
+			computation.handleInvalid(event, scheduled);
+		}
+		localValueComputations.clear();
+
+		// We need to cleanup computations recursively see bug 468048
+		for (EclipseContext cc : c.getChildren()) {
+			invalidateLocalComputations(cc, scheduled);
+		}
 	}
 }
