a72ce69a6663a1a3bf54b162e991b1f3c204112d
Tom Schindl <tom.schindl@bestsolution.at>
2015-06-17 12:02:09 +0200

    fixed warnings

--- bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/stage/DefaultTrimmedWindowPane.java
+++ bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/stage/DefaultTrimmedWindowPane.java
@@ -23,7 +23,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
