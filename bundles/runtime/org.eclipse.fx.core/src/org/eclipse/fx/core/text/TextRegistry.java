c1b7405ce01482bb9336413c347a06679bbe0677
Tom Schindl <tom.schindl@bestsolution.at>
2015-08-21 10:10:34 +0200

    Bug 475563 - Add TextRegistry#getSupplierByKey as default method

--- bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/text/TextRegistry.java
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/text/TextRegistry.java
@@ -10,35 +10,94 @@
  *******************************************************************************/
 package org.eclipse.fx.core.text;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import org.eclipse.fx.core.Subscription;
+import org.eclipse.fx.core.log.LoggerCreator;
 
 /**
  * Base interface for text registries
- * 
+ *
  * @since 2.0
  */
 public interface TextRegistry {
 	/**
 	 * Register a consumer and supplier
-	 * <pre>
-	 *   &#064;Inject
-	 *   private TextRegistry reg; // Subclass of AbstractMessageRegistry
 	 *
-	 *   TitledPane tb = new TitledPane();
-	 *   reg.register(tb::setText, () -> "Hello");
+	 * <pre>
+	 * &#064;Inject
+	 * private TextRegistry reg; // Subclass of AbstractMessageRegistry
+	 *
+	 * TitledPane tb = new TitledPane();
+	 * reg.register(tb::setText, () -> "Hello");
 	 * </pre>
 	 *
-	 * @param consumer the consumer the value is set on
-	 * @param supplier the supplier
+	 * @param consumer
+	 *            the consumer the value is set on
+	 * @param supplier
+	 *            the supplier
 	 * @return the subscription
 	 */
 	public Subscription register(Consumer<String> consumer, Supplier<String> supplier);
-	
+
 	/**
 	 * Force an update of all bindings
 	 */
 	public void updateBindings();
+
+	/**
+	 * Get the supplier by the key
+	 *
+	 * @param key
+	 *            the key
+	 * @param values
+	 *            the dynamic values
+	 * @return the supplier or <code>null</code>
+	 * @since 2.1
+	 */
+	@SuppressWarnings("null")
+	default public java.util.function.Supplier<String> getSupplierByKey(String key, Object... values) {
+		//FIXME Make none default in 3.0
+		if( values.length == 0 ) {
+			try {
+				Method m = getClass().getMethod(key);
+				return new Supplier<String>() {
+
+					@Override
+					public String get() {
+						try {
+							return (String) m.invoke(this);
+						} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				};
+			} catch (NoSuchMethodException | SecurityException e) {
+				LoggerCreator.createLogger(getClass()).error("No translation for '"+key+"' is found");  //$NON-NLS-1$//$NON-NLS-2$
+			}
+		} else {
+			try {
+				Stream.of(getClass().getMethods())
+					.filter(m -> (key + "_supplier").equals(m.getName())) //$NON-NLS-1$
+					.findFirst().map( m -> new Supplier<String>() {
+
+					@Override
+					public String get() {
+						try {
+							return (String) m.invoke(this, values);
+						} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				});
+			} catch (SecurityException e) {
+				LoggerCreator.createLogger(getClass()).error("No translation for '"+key+"' is found");  //$NON-NLS-1$//$NON-NLS-2$
+			}
+		}
+		return null;
+	}
 }
