7c39d002e511c8b5dd1b37f385828b85407cde9b
Tom Schindl <tom.schindl@bestsolution.at>
2015-08-26 15:55:11 +0200

    Bug 475925 - Add custom @Preference to not depend on di.extensions and IEclipsePreferences

--- bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/preferences/Preference.java
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/preferences/Preference.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.core.preferences;
 
 import java.lang.annotation.Documented;
@@ -8,11 +18,42 @@ import java.lang.annotation.Target;
 
 import javax.inject.Qualifier;
 
+/**
+ * <p>
+ * Annotation to inject a preference value.
+ * </p>
+ * The injected type can be:
+ * <ul>
+ * <li>"primitive": String, Boolean, boolean, Integer, int, Double, double, Float, float, Long, long</li>
+ * <li>{@link Value}: where the type argument can be String, Boolean, Integer, Double, Float, Long</li>
+ * </ul>
+ * <p>
+ * If you value {@link Value} the injected value must be exactly of this type
+ * </p>
+ * Examples
+ * <pre>
+ * class Test {
+ *   &#64;Inject
+ *   &#64;Preference("value")
+ *   int value;
+ *
+ *   &#64;Inject
+ *   &#64;Preference("value")
+ *   Value&lt;Integer&gt; value;
+ * }
+ * </pre>
+ */
 @Qualifier
 @Documented
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Preference {
+	/**
+	 * @return path to the node
+	 */
 	String nodePath() default "";
+	/**
+	 * @return the key in the node
+	 */
 	String key();
 }
