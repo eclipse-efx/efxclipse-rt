f6b90540733ae6fc95bd7193766beb5d474be09d
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-01 13:44:01 +0200

    make editor opening extensible

---
+++ bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/EditorOpenerTypeProvider.java
@@ -0,0 +1,7 @@
+package org.eclipse.fx.code.editor.services;
+
+import org.eclipse.fx.core.TypeProviderService;
+
+public interface EditorOpenerTypeProvider extends TypeProviderService<String, EditorOpener> {
+
+}
--- bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/TextEditorOpener.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.fx.code.editor.services;
-
-public interface TextEditorOpener {
-	public void openEditor(String uri);
-}
