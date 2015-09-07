a2729a529e41924b4412fbc907954b347f6073ab
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-07 10:15:03 +0200

    Bug 476758 - IconFontLookup should use fx.core.Util.lookupService

--- bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/image/spi/IconFontLookup.java
+++ bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/image/spi/IconFontLookup.java
@@ -10,17 +10,11 @@
  *******************************************************************************/
 package org.eclipse.fx.ui.controls.image.spi;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.ServiceLoader;
+
+import org.eclipse.fx.core.Util;
 
 import javafx.scene.text.Font;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 
 /**
  * Icon look up
@@ -29,74 +23,16 @@ public class IconFontLookup {
 
 	/**
 	 * Get a provider for the given font
-	 * 
+	 *
 	 * @param font
 	 *            the font
 	 * @return the provider
 	 */
 	public static IconFontProvider getProviderForFont(Font font) {
-		IconFontProvider result = null;
-		try {
-			Class<?> util = Class.forName("org.osgi.framework.FrameworkUtil"); //$NON-NLS-1$
-			if (util != null) {
-				final Method method = util.getMethod("getBundle", Class.class); //$NON-NLS-1$
-				Bundle bundle = (Bundle) method.invoke(null, IconFontLookup.class);
-				if (bundle != null) {
-					result = getProviderForFontOSGi(bundle, font);
-					return result;
-				}
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		}
-
-		if (result == null) {
-			result = getProviderForFontSPI(font);
-		}
-
-		return result;
-	}
-
-	private static IconFontProvider getProviderForFontOSGi(Bundle bundle, Font font) {
-		// System.err.println("getProviderForFontOSGi " + font);
-		IconFontProvider result = null;
-		try {
-			final BundleContext bundleContext = bundle.getBundleContext();
-			final Collection<ServiceReference<IconFontProvider>> serviceReferences = bundleContext.getServiceReferences(IconFontProvider.class, null);
-			for (ServiceReference<IconFontProvider> ref : serviceReferences) {
-				final IconFontProvider service = bundleContext.getService(ref);
-				if (service.getName().equals(font.getName())) {
-					result = service;
-					break;
-				}
-			}
-			// System.err.println("getProviderForFontOSGi => " + result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-
-	}
-
-	private static IconFontProvider getProviderForFontSPI(Font font) {
 		// System.err.println("getProviderForFontSPI " + font);
 		IconFontProvider result = null;
 
-		ServiceLoader<IconFontProvider> load = ServiceLoader.<IconFontProvider> load(IconFontProvider.class);
-		// System.err.println(" => " + load);
-		// load.forEach((r)->System.err.println(" * > " + r));
-
-		Iterator<IconFontProvider> it = load.iterator();
+		Iterator<IconFontProvider> it = Util.lookupServiceList(IconFontProvider.class).iterator();
 		while (it.hasNext()) {
 			IconFontProvider sup = it.next();
 			if (sup.getName().equals(font.getName())) {
@@ -107,6 +43,6 @@ public class IconFontLookup {
 
 		// System.err.println("getProviderForFontSPI => " + result);
 		return result;
-	}
 
+	}
 }
