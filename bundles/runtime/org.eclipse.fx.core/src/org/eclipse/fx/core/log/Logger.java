a1ffea677dfbbf39202d93e41853bd20b416ba31
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-08 00:02:16 +0200

    Bug 476820 - Add methods to provide log messages through Supplier<String>

--- bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/log/Logger.java
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/log/Logger.java
@@ -11,16 +11,19 @@
  *******************************************************************************/
 package org.eclipse.fx.core.log;
 
+import java.util.function.Supplier;
+
 import org.eclipse.jdt.annotation.NonNull;
 
 /**
- * Logger interface delegating to real log framework implementation like log4j, java.util.logging, ...
+ * Logger interface delegating to real log framework implementation like log4j,
+ * java.util.logging, ...
  */
 public interface Logger {
 
 	/**
 	 * logs a message in the specified log level.
-	 * 
+	 *
 	 * @param level
 	 *            the Log Level
 	 * @param message
@@ -29,8 +32,23 @@ public interface Logger {
 	public void log(@NonNull Level level, @NonNull String message);
 
 	/**
+	 * logs a message in the specified log level
+	 *
+	 * @param level
+	 *            the log level
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void log(@NonNull Level level, @NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(level)) {
+			log(level, messageSupplier.get());
+		}
+	}
+
+	/**
 	 * logs a message and an exception in the specified log level
-	 * 
+	 *
 	 * @param level
 	 *            the Log Level
 	 * @param message
@@ -41,16 +59,34 @@ public interface Logger {
 	public void log(@NonNull Level level, @NonNull String message, @NonNull Throwable t);
 
 	/**
+	 * logs a message and an exception in the specified log level
+	 *
+	 * @param level
+	 *            the Log Level
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the Exception
+	 * @since 2.2.0
+	 */
+	default void log(@NonNull Level level, @NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(level)) {
+			log(level, messageSupplier.get(), t);
+		}
+	}
+
+	/**
 	 * logs a formatted message in the specified log level.
 	 * <p>
-	 * For formatting {@link String#format(String, Object...)} is used. The following two method calls yield
-	 * the same result, but the logf call allows the underlying implementation to prevent the formatting if
-	 * the log level is disabled. <code><br>
+	 * For formatting {@link String#format(String, Object...)} is used. The
+	 * following two method calls yield the same result, but the logf call
+	 * allows the underlying implementation to prevent the formatting if the log
+	 * level is disabled. <code><br>
 	 *  logf(Level.TRACE, "%5.3f", num);<br>
 	 *  log(Level.TRACE, String.format("%5.3", num));
 	 * </code>
 	 * </p>
-	 * 
+	 *
 	 * @see String#format(String, Object...)
 	 * @param level
 	 *            the Log Level
@@ -64,14 +100,15 @@ public interface Logger {
 	/**
 	 * logs a formatted message in the specified log level.
 	 * <p>
-	 * For formatting {@link String#format(String, Object...)} is used. The following two method calls yield
-	 * the same result, but the logf call allows the underlying implementation to prevent the formatting if
-	 * the log level is disabled. <code><br>
+	 * For formatting {@link String#format(String, Object...)} is used. The
+	 * following two method calls yield the same result, but the logf call
+	 * allows the underlying implementation to prevent the formatting if the log
+	 * level is disabled. <code><br>
 	 *  logf(Level.TRACE, "%5.3f", num);<br>
 	 *  log(Level.TRACE, String.format("%5.3", num));
 	 * </code>
 	 * </p>
-	 * 
+	 *
 	 * @see String#format(String, Object...)
 	 * @param level
 	 *            the Log Level
@@ -85,8 +122,9 @@ public interface Logger {
 	public void logf(@NonNull Level level, @NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#TRACE}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#TRACE}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -94,8 +132,23 @@ public interface Logger {
 	public void trace(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#DEBUG}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#TRACE}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void trace(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.TRACE)) {
+			trace(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#DEBUG}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -103,8 +156,23 @@ public interface Logger {
 	public void debug(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#INFO}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#DEBUG}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void debug(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.DEBUG)) {
+			debug(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#INFO}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -112,8 +180,23 @@ public interface Logger {
 	public void info(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#WARNING}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#INFO}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void info(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.INFO)) {
+			info(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#WARNING}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -121,8 +204,23 @@ public interface Logger {
 	public void warning(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#ERROR}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#WARNING}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void warning(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.WARNING)) {
+			warning(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#ERROR}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -130,8 +228,23 @@ public interface Logger {
 	public void error(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String)} with {@link Level#FATAL}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#ERROR}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void error(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.ERROR)) {
+			error(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#FATAL}.
+	 *
 	 * @see #log(Level, String)
 	 * @param message
 	 *            the message
@@ -139,8 +252,23 @@ public interface Logger {
 	public void fatal(@NonNull String message);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#TRACE}.
-	 * 
+	 * convenience method for {@link #log(Level, String)} with
+	 * {@link Level#FATAL}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @since 2.2.0
+	 */
+	default void fatal(@NonNull Supplier<@NonNull String> messageSupplier) {
+		if (isEnabled(Level.FATAL)) {
+			fatal(messageSupplier.get());
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#TRACE}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -150,8 +278,25 @@ public interface Logger {
 	public void trace(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#DEBUG}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#TRACE}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the exception
+	 * @since 2.2.0
+	 */
+	default void trace(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.TRACE)) {
+			trace(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#DEBUG}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -161,8 +306,25 @@ public interface Logger {
 	public void debug(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#INFO}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#DEBUG}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the exception
+	 * @since 2.2.0
+	 */
+	default void debug(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.DEBUG)) {
+			debug(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#INFO}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -172,8 +334,25 @@ public interface Logger {
 	public void info(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#WARNING}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#INFO}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the exception
+	 * @since 2.2.0
+	 */
+	default void info(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.INFO)) {
+			info(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#WARNING}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -183,8 +362,25 @@ public interface Logger {
 	public void warning(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#ERROR}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#WARNING}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the exception
+	 * @since 2.2.0
+	 */
+	default void warning(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.WARNING)) {
+			warning(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#ERROR}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -194,8 +390,25 @@ public interface Logger {
 	public void error(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #log(Level, String, Throwable)} with {@link Level#FATAL}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#ERROR}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the Exception
+	 * @since 2.2.0
+	 */
+	default void error(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.ERROR)) {
+			error(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#FATAL}.
+	 *
 	 * @see #log(Level, String, Throwable)
 	 * @param message
 	 *            the message
@@ -205,8 +418,25 @@ public interface Logger {
 	public void fatal(@NonNull String message, @NonNull Throwable t);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#TRACE}.
-	 * 
+	 * convenience method for {@link #log(Level, String, Throwable)} with
+	 * {@link Level#FATAL}.
+	 *
+	 * @param messageSupplier
+	 *            the message supplier
+	 * @param t
+	 *            the Exception
+	 * @since 2.2.0
+	 */
+	default void fatal(@NonNull Supplier<@NonNull String> messageSupplier, @NonNull Throwable t) {
+		if (isEnabled(Level.FATAL)) {
+			fatal(messageSupplier.get(), t);
+		}
+	}
+
+	/**
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#TRACE}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -217,8 +447,9 @@ public interface Logger {
 	public void tracef(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#DEBUG}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#DEBUG}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -229,8 +460,9 @@ public interface Logger {
 	public void debugf(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#INFO}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#INFO}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -241,8 +473,9 @@ public interface Logger {
 	public void infof(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#WARNING}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#WARNING}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -253,8 +486,9 @@ public interface Logger {
 	public void warningf(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#ERROR}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#ERROR}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -265,8 +499,9 @@ public interface Logger {
 	public void errorf(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Object...)} with {@link Level#FATAL}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Object...)} with
+	 * {@link Level#FATAL}.
+	 *
 	 * @see #logf(Level, String, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -277,8 +512,9 @@ public interface Logger {
 	public void fatalf(@NonNull String pattern, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#TRACE}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#TRACE}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -291,8 +527,9 @@ public interface Logger {
 	public void tracef(@NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#DEBUG}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#DEBUG}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -305,8 +542,9 @@ public interface Logger {
 	public void debugf(@NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#INFO}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#INFO}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -319,8 +557,9 @@ public interface Logger {
 	public void infof(@NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#WARNING}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#WARNING}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -333,8 +572,9 @@ public interface Logger {
 	public void warningf(@NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#ERROR}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#ERROR}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -347,8 +587,9 @@ public interface Logger {
 	public void errorf(@NonNull String pattern, @NonNull Throwable t, Object... args);
 
 	/**
-	 * convenience method for {@link #logf(Level, String, Throwable, Object...)} with {@link Level#FATAL}.
-	 * 
+	 * convenience method for {@link #logf(Level, String, Throwable, Object...)}
+	 * with {@link Level#FATAL}.
+	 *
 	 * @see #logf(Level, String, Throwable, Object...)
 	 * @see String#format(String, Object...)
 	 * @param pattern
@@ -362,7 +603,7 @@ public interface Logger {
 
 	/**
 	 * checks whether the given log level is enabled for this logger.
-	 * 
+	 *
 	 * @param level
 	 *            the log level to check against
 	 * @return true if the log level is enabled
@@ -376,26 +617,25 @@ public interface Logger {
 		/**
 		 * Trace: Use this log level when you want to track things line by line
 		 */
-		TRACE,
-		/**
-		 * Debug: Use this log level to help developers debug your application
-		 */
-		DEBUG,
-		/**
-		 * Info: Use this log level for normal log statements like (request received, ...)
-		 */
-		INFO,
-		/**
-		 * Warning: Use this log level for problems like informing about a resource pool is getting low, ...
-		 */
-		WARNING,
-		/**
-		 * Error: Use this log level if something goes wrong e.g. an SQL-Query could not be executed, ...
-		 */
-		ERROR,
-		/**
-		 * Fatal: Use this log level in case of a sever error
-		 */
+		TRACE, /**
+				 * Debug: Use this log level to help developers debug your
+				 * application
+				 */
+		DEBUG, /**
+				 * Info: Use this log level for normal log statements like
+				 * (request received, ...)
+				 */
+		INFO, /**
+				 * Warning: Use this log level for problems like informing about
+				 * a resource pool is getting low, ...
+				 */
+		WARNING, /**
+					 * Error: Use this log level if something goes wrong e.g. an
+					 * SQL-Query could not be executed, ...
+					 */
+		ERROR, /**
+				 * Fatal: Use this log level in case of a sever error
+				 */
 		FATAL
 	}
 }
