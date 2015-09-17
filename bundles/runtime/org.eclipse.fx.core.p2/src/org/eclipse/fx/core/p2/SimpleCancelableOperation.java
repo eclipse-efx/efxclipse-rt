97b2236e1f6ab208af85a703e47b878c8d1aff2f
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-17 18:27:19 +0200

    Bug 473238 - Use package dependencies instead of require bundle

--- bundles/runtime/org.eclipse.fx.core.p2/src/org/eclipse/fx/core/p2/SimpleCancelableOperation.java
+++ bundles/runtime/org.eclipse.fx.core.p2/src/org/eclipse/fx/core/p2/SimpleCancelableOperation.java
@@ -1,12 +1,7 @@
 package org.eclipse.fx.core.p2;
 
-import java.util.Optional;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
-import org.eclipse.fx.core.Status;
-import org.eclipse.fx.core.Status.State;
 import org.eclipse.fx.core.StatusException;
 import org.eclipse.fx.core.operation.CancelableOperation;
 import org.eclipse.jdt.annotation.NonNull;
@@ -19,26 +14,26 @@ public class SimpleCancelableOperation<T> implements CancelableOperation<T> {
 	private static final int COMPLETED_OK = 1;
 	private static final int COMPLETED_EXCEPTION = 2;
 	private static final int CANCLED = 3;
-	
+
 	@Nullable
 	private T object;
-	
+
 	@Nullable
 	private Consumer<@Nullable T> onCompleteConsumer;
-	
+
 	@Nullable
 	private StatusException exception;
 	@Nullable
 	private Consumer<@NonNull StatusException> onCompleteExceptionallyConsumer;
-	
+
 	@Nullable
 	private Runnable onCancelConsumer;
-	
-	
+
+
 	public SimpleCancelableOperation(@NonNull Runnable cancelable) {
 		this.cancelable = cancelable;
 	}
-	
+
 	public synchronized void completed(@Nullable T object) {
 		this.object = object;
 		this.state = COMPLETED_OK;
@@ -46,7 +41,7 @@ public class SimpleCancelableOperation<T> implements CancelableOperation<T> {
 			this.onCompleteConsumer.accept(object);
 		}
 	}
-	
+
 	public synchronized void completeExceptionally(@NonNull StatusException t) {
 		this.exception = t;
 		this.state = COMPLETED_EXCEPTION;
@@ -54,11 +49,11 @@ public class SimpleCancelableOperation<T> implements CancelableOperation<T> {
 			this.onCompleteExceptionallyConsumer.accept(t);
 		}
 	}
-	
+
 	@Override
 	public synchronized CancelableOperation<T> onComplete(@NonNull Consumer<@Nullable T> consumer) {
 		if( this.state == COMPLETED_OK ) {
-			consumer.accept(this.object);				
+			consumer.accept(this.object);
 		} else {
 			this.onCompleteConsumer = consumer;
 		}
@@ -69,7 +64,7 @@ public class SimpleCancelableOperation<T> implements CancelableOperation<T> {
 	public synchronized CancelableOperation<T> onException(@NonNull Consumer<@NonNull StatusException> consumer) {
 		if( this.state == COMPLETED_EXCEPTION ) {
 			if( this.exception != null ) {
-				consumer.accept(this.exception);	
+				consumer.accept(this.exception);
 			}
 		} else {
 			this.onCompleteExceptionallyConsumer = consumer;
@@ -92,7 +87,7 @@ public class SimpleCancelableOperation<T> implements CancelableOperation<T> {
 		this.cancelable.run();
 		this.state = CANCLED;
 		if( this.onCancelConsumer != null ) {
-			this.onCancelConsumer.run();	
+			this.onCancelConsumer.run();
 		}
 	}
 
