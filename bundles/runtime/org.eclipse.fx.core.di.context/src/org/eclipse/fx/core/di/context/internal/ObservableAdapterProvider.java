4be6f2ed532c7e52950bab6f43bcbf02ec51a3d5
Thorsten Hake <eclipse@thorsten-hake.com>
2015-09-17 15:19:46 +0200

    Bug 477417 and incorrect access to IObservables having a Realm
    The ObservableAdapterProvider now creates IObservables that are always
    in sync with the ContextBoundValue it adapts. Additionally the realm of
    the IObservableValue will be respected when sending out events.
    
    Change-Id: Ic01acf2fbe692be7cdb2fdea98fc5f466a0cfa3f
    Signed-off-by: Thorsten Hake <eclipse@thorsten-hake.com>

--- bundles/runtime/org.eclipse.fx.core.di.context/src/org/eclipse/fx/core/di/context/internal/ObservableAdapterProvider.java
+++ bundles/runtime/org.eclipse.fx.core.di.context/src/org/eclipse/fx/core/di/context/internal/ObservableAdapterProvider.java
@@ -7,25 +7,96 @@
  *
  * Contributors:
  *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *     Thorsten Hake <mail@thorsten-hake.com> - fix for Bug 477417
  *******************************************************************************/
 package org.eclipse.fx.core.di.context.internal;
 
+import org.eclipse.core.databinding.observable.Diffs;
 import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.value.AbstractObservableValue;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
-import org.eclipse.core.databinding.observable.value.IValueChangeListener;
-import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
-import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.fx.core.Callback;
+import org.eclipse.fx.core.Subscription;
 import org.eclipse.fx.core.adapter.AdapterProvider;
 import org.eclipse.fx.core.adapter.AdapterService.ValueAccess;
 import org.eclipse.fx.core.di.ContextBoundValue;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * Eclipse Databinding Observable Adapter
  */
 @SuppressWarnings("rawtypes")
 public class ObservableAdapterProvider implements AdapterProvider<ContextBoundValue, IObservableValue> {
+	/**
+	 * Private class that adapts represents an ContextBoundValue as an IObservableValue.
+	 *
+	 */
+	private static class ContextBoundObservableValue extends AbstractObservableValue{
+		private ContextBoundValue sourceObject;
+		private Object valueType;
+		
+		private Subscription currentSubscription;
+		
+		final class ContextBoundValueChanged implements Callback{
+			private Object lastValue = doGetValue();
+			
+			public ContextBoundValueChanged() {
+				super();
+			}
+			
+			@SuppressWarnings("synthetic-access")
+			@Override
+			public void call(@Nullable Object value) {
+				//Make sure that we fire events in the correct realm
+				getRealm().exec(() -> {
+					if(this.lastValue != value){
+						fireValueChange(Diffs.createValueDiff(this.lastValue, this.lastValue = value));
+					}
+				});
+			}
+			
+		}
+		
+		public ContextBoundObservableValue(Realm realm, ContextBoundValue boundValue) {
+			this(realm,boundValue,null);
+			
+		}
+		public ContextBoundObservableValue( Realm realm, ContextBoundValue boundValue, Object valueType) {
+			super(realm);
+			this.sourceObject = boundValue;
+			this.valueType = valueType;
+		}
+		
+		@Override
+		public Object getValueType() {				
+			return this.valueType;
+		}
 
+		@Override
+		protected Object doGetValue() {
+			return this.sourceObject.getValue();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		protected void doSetValue(Object value) {
+			 this.sourceObject.publish(value);
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		protected void firstListenerAdded(){
+			this.currentSubscription = this.sourceObject.subscribeOnValueChange(new ContextBoundValueChanged());
+		}
+		@Override
+		protected void lastListenerRemoved(){
+			if(this.currentSubscription != null){
+				this.currentSubscription.dispose();
+				this.currentSubscription = null;
+			}
+		}
+	}
+	
 	@Override
 	public Class<ContextBoundValue> getSourceType() {
 		return ContextBoundValue.class;
@@ -41,7 +112,7 @@ public class ObservableAdapterProvider implements AdapterProvider<ContextBoundVa
 		return true;
 	}
 
-	@SuppressWarnings({ "unchecked", "null" })
+	@SuppressWarnings({ "unchecked"})
 	@Override
 	public IObservableValue adapt(final ContextBoundValue sourceObject, Class<IObservableValue> targetType, ValueAccess... valueAccess) {
 		Realm r = null;
@@ -68,22 +139,8 @@ public class ObservableAdapterProvider implements AdapterProvider<ContextBoundVa
 			};
 		}
 		
-		final WritableValue w = new WritableValue(r);
-		w.setValue(sourceObject.getValue());
-		w.addValueChangeListener(new IValueChangeListener() {
-			
-			@Override
-			public void handleValueChange(ValueChangeEvent event) {
-				sourceObject.publish(event.diff.getNewValue());
-			}
-		});
-		sourceObject.subscribeOnValueChange(new Callback() {
-
-			@Override
-			public void call(Object value) {
-				w.setValue(value);
-			}
-		});
+		final IObservableValue w = new ContextBoundObservableValue(r,sourceObject);
+	
 		sourceObject.subscribeOnDispose(new Callback<Void>() {
 
 			@Override
