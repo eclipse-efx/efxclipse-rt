ca7a9f790ba43791256ba475adfe9588f9eeb699
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-22 09:37:16 +0200

    Bug 478028 - Add functional interfaces for relation operations for primitives (int, double)

---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/function/DoubleRelationOperation.java
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.core.function;
+
+/**
+ * Relation operations for doubles
+ * <ul>
+ * <li>&lt; - lower than</li>
+ * <li>&gt; - greater than</li>
+ * <li>&lt;= - lower than or equal</li>
+ * <li>&gt;= - greater than or equal</li>
+ * <li>== - equal</li>
+ * <li>!= - not equal</li>
+ * </ul>
+ *
+ * To create relation operations one uses the static helpers
+ * <pre><code>
+ * DoubleRelationOperation r = DoubleRelationOperation::lt
+ * if( r.apply(10.0, 11.0) ) {
+ * 	// 10.0 is lower than 11.0
+ * }
+ * </code></pre>
+ *
+ * @since 2.2
+ */
+public interface DoubleRelationOperation {
+	/**
+	 * Execute the relation
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public boolean apply(double i, double j);
+
+	/**
+	 * Check if i lower than j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i < j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean lt(double i, double j) {
+		return i < j;
+	}
+
+	/**
+	 * Check if i lower than or equal j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i <= j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean lte(double i, double j) {
+		return i <= j;
+	}
+
+	/**
+	 * Check if i greater than j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i > j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean gt(double i, double j) {
+		return i > j;
+	}
+
+	/**
+	 * Check if i greater than or equal j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i >= j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean gte(double i, double j) {
+		return i >= j;
+	}
+
+	/**
+	 * Check if i equal to j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i == j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean eq(double i, double j) {
+		return i == j;
+	}
+
+	/**
+	 * Check if i not equal to j
+	 *
+	 * <pre>
+	 * <code>
+	 *  i != j
+	 * </code>
+	 * </pre>
+	 *
+	 * @param i
+	 *            the first operand
+	 * @param j
+	 *            the 2nd operand
+	 * @return the result
+	 */
+	public static boolean neq(double i, double j) {
+		return i != j;
+	}
+}
