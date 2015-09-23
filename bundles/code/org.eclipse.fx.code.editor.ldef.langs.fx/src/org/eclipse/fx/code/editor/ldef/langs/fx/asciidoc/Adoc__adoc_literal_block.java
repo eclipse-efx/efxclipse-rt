93ef9ac8995c62f3e1471ee172de06a723e717b8
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-23 15:43:16 +0200

    fixed ascii doc

---
+++ bundles/code/org.eclipse.fx.code.editor.ldef.langs.fx/src/org/eclipse/fx/code/editor/ldef/langs/fx/asciidoc/Adoc__adoc_literal_block.java
@@ -0,0 +1,11 @@
+package org.eclipse.fx.code.editor.ldef.langs.fx.asciidoc;
+
+public class Adoc__adoc_literal_block extends org.eclipse.jface.text.rules.RuleBasedScanner {
+	public Adoc__adoc_literal_block() {
+		org.eclipse.jface.text.rules.Token adoc_literalToken = new org.eclipse.jface.text.rules.Token(new org.eclipse.jface.text.TextAttribute("adoc.adoc_literal"));
+		setDefaultReturnToken(adoc_literalToken);
+		org.eclipse.jface.text.rules.IRule[] rules = new org.eclipse.jface.text.rules.IRule[0];
+
+		setRules(rules);
+	}
+}
