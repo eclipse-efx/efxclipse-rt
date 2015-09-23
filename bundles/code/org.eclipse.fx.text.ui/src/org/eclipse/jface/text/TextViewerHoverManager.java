0efd45ac4eaed2faa6978969abe5d72cb7254534
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-23 15:37:12 +0200

    comment hover support

--- bundles/code/org.eclipse.fx.text.ui/src/org/eclipse/jface/text/TextViewerHoverManager.java
+++ bundles/code/org.eclipse.fx.text.ui/src/org/eclipse/jface/text/TextViewerHoverManager.java
@@ -1,11 +1,7 @@
 package org.eclipse.jface.text;
 
 import org.eclipse.fx.ui.controls.styledtext.StyledTextArea;
-import org.eclipse.fx.ui.controls.styledtext.TextHoverEvent;
-import org.eclipse.jdt.annotation.Nullable;
 
-import javafx.geometry.Point2D;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.PopupWindow;
 
@@ -32,30 +28,30 @@ public class TextViewerHoverManager {
 	}
 
 	public void install(StyledTextArea styledTextArea) {
-		styledTextArea.addEventHandler(TextHoverEvent.HOVER, e -> {
-			if( e.getOffset() > 0 ) {
-				final ITextHover hover= textViewer.getTextHover(e.getOffset(), /*getHoverEventStateMask()*/ ITextViewerExtension2.DEFAULT_HOVER_STATE_MASK);
-				if( hover != null ) {
-					String text = hover.getHoverInfo(textViewer, hover.getHoverRegion(textViewer, e.getOffset()));
-					if( text != null && ! text.isEmpty() ) {
-						Label value = new Label(text);
-						value.getStyleClass().add("styled-text-hover-text");
-						root.setCenter(value);
-						Point2D locationAtOffset = textViewer.getTextWidget().getLocationAtOffset(e.getOffsetTokenStart());
-						double x = e.getScreenX();
-						if( locationAtOffset != null ) {
-							x = textViewer.getTextWidget().localToScreen(locationAtOffset.getX(),0).getX();
-						}
-						popup.show(textViewer.getTextWidget().getScene().getWindow(), x, e.getScreenY()+5);
-					} else {
-						popup.hide();
-					}
-				} else {
-					popup.hide();
-				}
-			} else {
-				popup.hide();
-			}
-		});
+//		styledTextArea.addEventHandler(TextHoverEvent.HOVER, e -> {
+//			if( e.getOffset() > 0 ) {
+//				final ITextHover hover= textViewer.getTextHover(e.getOffset(), /*getHoverEventStateMask()*/ ITextViewerExtension2.DEFAULT_HOVER_STATE_MASK);
+//				if( hover != null ) {
+//					String text = hover.getHoverInfo(textViewer, hover.getHoverRegion(textViewer, e.getOffset()));
+//					if( text != null && ! text.isEmpty() ) {
+//						Label value = new Label(text);
+//						value.getStyleClass().add("styled-text-hover-text");
+//						root.setCenter(value);
+//						Point2D locationAtOffset = textViewer.getTextWidget().getLocationAtOffset(e.getOffsetTokenStart());
+//						double x = e.getScreenX();
+//						if( locationAtOffset != null ) {
+//							x = textViewer.getTextWidget().localToScreen(locationAtOffset.getX(),0).getX();
+//						}
+//						popup.show(textViewer.getTextWidget().getScene().getWindow(), x, e.getScreenY()+5);
+//					} else {
+//						popup.hide();
+//					}
+//				} else {
+//					popup.hide();
+//				}
+//			} else {
+//				popup.hide();
+//			}
+//		});
 	}
 }
