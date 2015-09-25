5d87b74800c695d249a151c7913cdddaf59f60f3
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-25 13:26:58 +0200

    Bug 478403 - Provide a possibility to have something like a IContextFunction who knows where the injection is done on

---
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/TypeTypeProviderService.java
@@ -0,0 +1,8 @@
+package org.eclipse.fx.core;
+
+public interface TypeTypeProviderService<T> extends TypeProviderService<Class<?>, T> {
+	/**
+	 * If used with dependency injection and @LocalInstance
+	 */
+	public static final String DI_KEY = "localInstanceOwnerType"; //$NON-NLS-1$
+}
--- bundles/runtime/org.eclipse.fx.ui.workbench.base/src/org/eclipse/fx/ui/workbench/base/internal/ModelServiceImpl.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.base/src/org/eclipse/fx/ui/workbench/base/internal/ModelServiceImpl.java
@@ -13,12 +13,17 @@ package org.eclipse.fx.ui.workbench.base.internal;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.fx.core.TypeProviderService;
+import org.eclipse.fx.core.TypeTypeProviderService;
 import org.eclipse.fx.ui.workbench.services.ModelService;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
@@ -29,14 +34,14 @@ import org.osgi.service.component.annotations.Component;
  * Default implementation of the {@link ModelService}
  */
 @Component
-public class ModelServiceImpl implements ModelService {
+public class ModelServiceImpl implements ModelService, TypeTypeProviderService<ModelService.ModelElementFactory> {
 
 	@Override
 	public @NonNull String getUniqueId(@NonNull MApplicationElement element) {
 		EObject eo = (EObject) element;
 		String id = ((XMIResource) eo.eResource()).getID(eo);
-		if( id == null ) {
-			throw new IllegalStateException("Unable to locate ID for '"+element+"'");  //$NON-NLS-1$//$NON-NLS-2$
+		if (id == null) {
+			throw new IllegalStateException("Unable to locate ID for '" + element + "'"); //$NON-NLS-1$//$NON-NLS-2$
 		}
 		return id;
 	}
@@ -44,24 +49,48 @@ public class ModelServiceImpl implements ModelService {
 	@SuppressWarnings("unchecked")
 	@Override
 	public <M extends MApplicationElement> @Nullable M getElementInstance(@NonNull MApplication m, @NonNull String uniqueId) {
-		return (M) ((XMIResource) ((EObject)m).eResource()).getEObject(uniqueId);
+		return (M) ((XMIResource) ((EObject) m).eResource()).getEObject(uniqueId);
 	}
 
 	@Override
 	public MApplication getRoot(@NonNull MApplicationElement e) {
-		Resource eResource = ((EObject)e).eResource();
+		Resource eResource = ((EObject) e).eResource();
 		return eResource != null ? (MApplication) eResource.getContents().get(0) : null;
 	}
 
-	static class ModelElementFactoryImpl implements ModelElementFactory {
+	@Override
+	public Class<? extends ModelElementFactory> getType(Class<?> s) {
+		return ModelElementFactoryImpl.class;
+	}
+
+	@Override
+	public boolean test(Class<?> t) {
+		return t == ModelElementFactory.class;
+	}
+
+	/**
+	 * Factory to create model elements
+	 */
+	public static class ModelElementFactoryImpl implements ModelElementFactory {
 		@NonNull
 		private final EModelService modelService;
 		@NonNull
 		private final Supplier<@NonNull String> contributor;
 
-		public ModelElementFactoryImpl(@NonNull EModelService modelService, @NonNull Supplier<@NonNull String> contributor) {
+
+
+		/**
+		 * Create a new instance
+		 *
+		 * @param modelService
+		 *            the model service
+		 * @param ownerType
+		 *            the owner type
+		 */
+		@Inject
+		public ModelElementFactoryImpl(@NonNull EModelService modelService, @Named(TypeTypeProviderService.DI_KEY) @NonNull Class<?> ownerType) {
 			this.modelService = modelService;
-			this.contributor = contributor;
+			this.contributor = _getBundleNameSupplier(ownerType);
 		}
 
 		@Override
@@ -82,7 +111,7 @@ public class ModelServiceImpl implements ModelService {
 		@Override
 		public <T extends MApplicationElement> @NonNull T createModelElement(@NonNull Class<@NonNull T> clazz, @NonNull Supplier<@NonNull String> contributor, Function<@NonNull T, @NonNull T> processor) {
 			T element = this.modelService.createModelElement(clazz);
-			element.setContributorURI("platform:/plugin/"+ contributor); //$NON-NLS-1$
+			element.setContributorURI("platform:/plugin/" + contributor); //$NON-NLS-1$
 			element = processor.apply(element);
 			return element;
 		}
@@ -110,21 +139,29 @@ public class ModelServiceImpl implements ModelService {
 
 	@Override
 	public @NonNull ModelElementFactory createModelElementFactory(@NonNull Class<?> owner, @NonNull EModelService modelService) {
-		return new ModelElementFactoryImpl(modelService, getBundleNameSupplier(owner));
+		return new ModelElementFactoryImpl(modelService, owner);
 	}
 
-	@Override
-	public @NonNull String getBundleName(Class<?> clazz) {
+	public static @NonNull String _getBundleName(Class<?> clazz) {
 		String bundleId = FrameworkUtil.getBundle(clazz).getSymbolicName();
-		if( bundleId == null ) {
-			throw new IllegalStateException("Could not extract the bundle name from the '"+clazz+"'");  //$NON-NLS-1$//$NON-NLS-2$
+		if (bundleId == null) {
+			throw new IllegalStateException("Could not extract the bundle name from the '" + clazz + "'"); //$NON-NLS-1$//$NON-NLS-2$
 		}
 		return bundleId;
 	}
 
+	public static @NonNull Supplier<@NonNull String> _getBundleNameSupplier(Class<?> clazz) {
+		String id = _getBundleName(clazz);
+		return () -> id;
+	}
+
+	@Override
+	public @NonNull String getBundleName(Class<?> clazz) {
+		return _getBundleName(clazz);
+	}
+
 	@Override
 	public @NonNull Supplier<@NonNull String> getBundleNameSupplier(Class<?> clazz) {
-		String id = getBundleName(clazz);
-		return () -> id;
+		return _getBundleNameSupplier(clazz);
 	}
 }
