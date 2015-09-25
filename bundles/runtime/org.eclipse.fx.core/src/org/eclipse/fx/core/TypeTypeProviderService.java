36f80fe29313bf03ce67c592586070765ddb35b3
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-25 15:27:00 +0200

    Bug 478403 - Provide a possibility to have something like a IContextFunction who knows where the injection is done on

--- bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/TypeTypeProviderService.java
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/TypeTypeProviderService.java
@@ -1,5 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.core;
 
+/**
+ * Service who provides a real type for an interface/abstract type
+ *
+ * @param <T>
+ *            the type
+ */
 public interface TypeTypeProviderService<T> extends TypeProviderService<java.lang.reflect.Type, T> {
 	/**
 	 * If used with dependency injection and @LocalInstance
