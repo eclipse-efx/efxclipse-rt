d9a82582e670eb75513bc0492198b85738d6435b
Tom Schindl <tom.schindl@bestsolution.at>
2015-10-17 00:03:48 +0200

    new prefs

--- bundles/code/org.eclipse.fx.code.editor.fx/src/org/eclipse/fx/code/editor/fx/services/SourceUnit.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.fx.code.editor.fx.services;
-
-import org.eclipse.jface.text.source.AnnotationModel;
-
-public interface SourceUnit {
-	public Outline getOutline();
-	public AnnotationModel getAnnotationModel();
-	public ProposalComputer getProposalComputer();
-}
