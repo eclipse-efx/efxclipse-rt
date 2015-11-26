6526aa119fc83f80b82661aaf9816d055414e5bf
Tom Schindl <tom.schindl@bestsolution.at>
2015-11-26 11:40:04 +0100

    added javadoc

--- bundles/runtime/org.eclipse.fx.ui.services/src/org/eclipse/fx/ui/services/dialog/LightWeightDialogService.java
+++ bundles/runtime/org.eclipse.fx.ui.services/src/org/eclipse/fx/ui/services/dialog/LightWeightDialogService.java
@@ -11,16 +11,49 @@
 package org.eclipse.fx.ui.services.dialog;
 
 import org.eclipse.fx.ui.controls.stage.Frame;
+
 import javafx.scene.Node;
 
+/**
+ * Service to open a lightweight dialog
+ */
+@SuppressWarnings("restriction")
 public interface LightWeightDialogService {
+	/**
+	 * The modality scope
+	 */
 	public enum ModalityScope {
 		// APPLICATION,
-		WINDOW,
-		PERSPECTIVE,
+		/**
+		 * Window modal
+		 */
+		WINDOW, /**
+				 * Perspective modal
+				 */
+		PERSPECTIVE, /**
+						 * Part modal
+						 */
 		PART
 	}
-	
+
+	/**
+	 * Open the given dialog class
+	 *
+	 * @param dialogClass
+	 *            the dialog to open
+	 * @param scope
+	 *            the scope
+	 * @return the dialog instance
+	 */
 	public <T extends Node & Frame> T openDialog(Class<T> dialogClass, ModalityScope scope);
+
+	/**
+	 * Open the given dialog instance in the given scope
+	 *
+	 * @param dialog
+	 *            the dialog
+	 * @param scope
+	 *            the scope
+	 */
 	public <T extends Node & Frame> void openDialog(T dialog, ModalityScope scope);
 }
