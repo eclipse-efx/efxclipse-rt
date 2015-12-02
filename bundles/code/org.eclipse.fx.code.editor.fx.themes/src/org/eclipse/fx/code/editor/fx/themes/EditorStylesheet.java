037c69ffcefcd260292bafdc02e72a613401bfc9
Tom Schindl <tom.schindl@bestsolution.at>
2015-12-02 21:26:23 +0100

    Bug 483520 - Add support for typescript

--- bundles/code/org.eclipse.fx.code.editor.fx.themes/src/org/eclipse/fx/code/editor/fx/themes/EditorStylesheet.java
+++ bundles/code/org.eclipse.fx.code.editor.fx.themes/src/org/eclipse/fx/code/editor/fx/themes/EditorStylesheet.java
@@ -15,7 +15,7 @@ public abstract class EditorStylesheet implements Stylesheet {
 	@Override
 	public URL getURL(Theme t) {
 		URL url = null;
-		if( "dark".equals(t.getId()) ) {
+		if( "dark".equals(t.getId()) || "theme.dark".equals(t.getId()) ) {
 			url = getClass().getClassLoader().getResource("css/dark-highlight.css");
 		} else {
 			url = getClass().getClassLoader().getResource("css/highlight.css");
