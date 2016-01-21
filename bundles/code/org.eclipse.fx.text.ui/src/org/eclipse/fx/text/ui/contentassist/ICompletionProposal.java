fae50f5d49cbbd5441dc2e30f5bfccf120ffbffd
Christoph Caks <ccaks@bestsolution.at>
2016-01-21 16:28:24 +0100

    Bug 486269 - StyledTextArea implement Undo/Redo Support
     reverted ContentAssist stuff

--- bundles/code/org.eclipse.fx.text.ui/src/org/eclipse/fx/text/ui/contentassist/ICompletionProposal.java
+++ bundles/code/org.eclipse.fx.text.ui/src/org/eclipse/fx/text/ui/contentassist/ICompletionProposal.java
@@ -17,40 +17,9 @@ import org.eclipse.jface.text.IDocument;
 
 public interface ICompletionProposal {
 	public CharSequence getLabel();
-
-	public String getHoverInfo();
-
 	public Node getGraphic();
 //	public List<String> getStyles();
 
-	/**
-	 * Inserts the proposed completion into the given document.
-	 *
-	 * @param document the document into which to insert the proposed completion
-	 */
 	public void apply(IDocument document);
-
-	/**
-	 * Returns the new selection after the proposal has been applied to
-	 * the given document in absolute document coordinates. If it returns
-	 * <code>null</code>, no new selection is set.
-	 *
-	 * A document change can trigger other document changes, which have
-	 * to be taken into account when calculating the new selection. Typically,
-	 * this would be done by installing a document listener or by using a
-	 * document position during {@link #apply(IDocument)}.
-	 *
-	 * @param document the document into which the proposed completion has been inserted
-	 * @return the new selection
-	 */
 	public TextSelection getSelection(IDocument document);
-
-	/**
-	 * Returns optional context information associated with this proposal.
-	 * The context information will automatically be shown if the proposal
-	 * has been applied.
-	 *
-	 * @return the context information for this proposal or <code>null</code>
-	 */
-	IContextInformation getContextInformation();
 }
