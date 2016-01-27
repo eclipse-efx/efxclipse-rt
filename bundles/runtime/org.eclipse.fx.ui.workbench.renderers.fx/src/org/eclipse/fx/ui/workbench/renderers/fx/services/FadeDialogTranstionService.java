a5519942b49da0fb43d0dcedbad5e3a01c72cd5f
Tom Schindl <tom.schindl@bestsolution.at>
2016-01-27 23:50:30 +0100

    fix copyright doc

--- bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/services/FadeDialogTranstionService.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.renderers.fx/src/org/eclipse/fx/ui/workbench/renderers/fx/services/FadeDialogTranstionService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.ui.workbench.renderers.fx.services;
 
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
@@ -9,7 +19,7 @@ import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public abstract class FadeDialogTranstionService implements LightweightDialogTransitionService {
-	
+
 	@Override
 	public void showDialog(MUIElement container, Pane containerNode, Pane dialogOverlayContainer, Node grayArea, Node dialog, Runnable finished) {
 		dialogOverlayContainer.setOpacity(0);
@@ -18,15 +28,15 @@ public abstract class FadeDialogTranstionService implements LightweightDialogTra
 		fd.setToValue(1);
 		configureFadeIn(fd);
 		if( finished != null ) {
-			fd.setOnFinished( e -> finished.run());	
+			fd.setOnFinished( e -> finished.run());
 		}
 		fd.play();
 	}
 
 	protected void configureFadeIn(FadeTransition transition) {
-		
+
 	}
-	
+
 	@Override
 	public void hideDialog(MUIElement container, Pane containerNode, Pane dialogOverlayContainer, Node grayArea, Node dialog, Runnable finished) {
 		FadeTransition fd = new FadeTransition(Duration.millis(300),dialogOverlayContainer);
@@ -34,12 +44,12 @@ public abstract class FadeDialogTranstionService implements LightweightDialogTra
 		fd.setToValue(0);
 		configureFadeOut(fd);
 		if( finished != null ) {
-			fd.setOnFinished( e -> finished.run());	
+			fd.setOnFinished( e -> finished.run());
 		}
 		fd.play();
 	}
-	
+
 	protected void configureFadeOut(FadeTransition transition) {
-		
+
 	}
 }
