e246f88e5c2541015b42cd5c2a40a3bdf96fa633
Tom Schindl <tom.schindl@bestsolution.at>
2016-03-23 12:05:00 +0100

    fix context id

--- bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/InputContextLookup.java
+++ bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/InputContextLookup.java
@@ -1,13 +1,11 @@
 package org.eclipse.fx.code.editor.services;
 
-import java.util.UUID;
-
 import org.eclipse.fx.code.editor.Input;
 import org.eclipse.fx.code.editor.InputContext;
 
 public interface InputContextLookup {
 	public static final InputContext DEFAULT = new InputContext() {
-		private final String ID = UUID.randomUUID().toString();
+		private final String ID = "__default_context";
 
 		@Override
 		public String getId() {
