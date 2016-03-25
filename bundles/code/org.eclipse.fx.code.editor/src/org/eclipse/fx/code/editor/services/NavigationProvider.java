96bc49b4ee37869e4d555a32a70dc005126aaa58
Tom Schindl <tom.schindl@bestsolution.at>
2016-03-25 09:12:05 +0100

    fixed javadoc

--- bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/NavigationProvider.java
+++ bundles/code/org.eclipse.fx.code.editor/src/org/eclipse/fx/code/editor/services/NavigationProvider.java
@@ -6,9 +6,25 @@ import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.fx.text.navigation.NavigationRegion;
 
-
+/**
+ * The navigation provider is responsible to provide informations about
+ * sensitive regions in the current document
+ *
+ * @since 2.4.0
+ */
 public interface NavigationProvider {
-
+	/**
+	 * @return all navigation regions known
+	 */
 	CompletableFuture<List<NavigationRegion>> getNavigationRegions();
+
+	/**
+	 * Search for the navigation at the provided caret offset or return an empty
+	 * optional of the current position is not a navigation sensitive region
+	 *
+	 * @param caretOffet
+	 *            the caret offset
+	 * @return optional containing the sensitive region
+	 */
 	CompletableFuture<Optional<NavigationRegion>> getNavigationRegion(int caretOffet);
 }
