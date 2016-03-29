436e90e14ff3bafe589a75a1d83dbb35d2767039
Tom Schindl <tom.schindl@bestsolution.at>
2016-03-30 00:49:06 +0200

    appropriately implemented point boundary impls

--- bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/text/TextUtil.java
+++ bundles/runtime/org.eclipse.fx.core/src/org/eclipse/fx/core/text/TextUtil.java
@@ -13,7 +13,6 @@ package org.eclipse.fx.core.text;
 import java.text.BreakIterator;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
-import java.util.Locale;
 
 import org.eclipse.fx.core.IntTuple;
 
@@ -23,7 +22,8 @@ import org.eclipse.fx.core.IntTuple;
  * @since 2.4.0
  */
 public class TextUtil {
-	private static final BreakIterator POSIX_ITERATOR = BreakIterator.getWordInstance(new Locale("en", "US", "POSIX")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+//TODO This would work with ICU
+//	private static final BreakIterator POSIX_ITERATOR = BreakIterator.getWordInstance(new Locale("en", "US", "POSIX"));   //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
 
 	/**
 	 * A {@link CharSequence} who can provide an {@link CharacterIterator}
@@ -49,9 +49,21 @@ public class TextUtil {
 	 * @return the end offset or {@link BreakIterator#DONE}
 	 */
 	public static int findWordEndOffset(IterableCharSequence content, int offset, boolean pointAsBoundary) {
-		BreakIterator wordInstance = pointAsBoundary ? POSIX_ITERATOR : BreakIterator.getWordInstance();
+		BreakIterator wordInstance = BreakIterator.getWordInstance();
 		wordInstance.setText(content.getIterator());
-		return wordInstance.following(offset);
+		int rv = wordInstance.following(offset);
+		if( rv != BreakIterator.DONE && pointAsBoundary ) {
+			String s = content.subSequence(offset, rv).toString();
+			int idx = s.indexOf('.');
+			if( idx >= 0 ) {
+				rv = offset + idx;
+			}
+
+			if( rv == offset ) {
+				rv = offset + 1;
+			}
+		}
+		return rv;
 	}
 
 	/**
@@ -66,9 +78,24 @@ public class TextUtil {
 	 * @return the start offset or or {@link BreakIterator#DONE}
 	 */
 	public static int findWordStartOffset(IterableCharSequence content, int offset, boolean pointAsBoundary) {
-		BreakIterator wordInstance = pointAsBoundary ? POSIX_ITERATOR : BreakIterator.getWordInstance();
+		BreakIterator wordInstance = BreakIterator.getWordInstance();
 		wordInstance.setText(content.getIterator());
-		return wordInstance.preceding(offset);
+		int rv = wordInstance.preceding(offset);
+
+		if( rv != BreakIterator.DONE && pointAsBoundary ) {
+			String s = content.subSequence(rv, offset).toString();
+			int idx = s.lastIndexOf('.');
+			if( idx > 0 ) {
+				rv += idx + 1;
+			}
+
+			// move before the point
+			if( rv == offset ) {
+				rv -= 1;
+			}
+		}
+
+		return rv;
 	}
 
 	/**
@@ -83,10 +110,26 @@ public class TextUtil {
 	 * @return a tuple of value representing start and end
 	 */
 	public static IntTuple findWordBounds(IterableCharSequence content, int offset, boolean pointAsBoundary) {
-		BreakIterator wordInstance = pointAsBoundary ? POSIX_ITERATOR : BreakIterator.getWordInstance();
+		BreakIterator wordInstance = BreakIterator.getWordInstance();
 		wordInstance.setText(content.getIterator());
 		int previous = wordInstance.preceding(offset);
 		int next = wordInstance.following(offset);
+
+		if( pointAsBoundary && previous != BreakIterator.DONE && next != BreakIterator.DONE ) {
+			String preMatch = content.subSequence(previous, offset).toString();
+			String postMatch = content.subSequence(offset, next).toString();
+
+			int idx = preMatch.lastIndexOf('.');
+			if( idx > 0 ) {
+				previous += idx + 1;
+			}
+
+			idx = postMatch.indexOf('.');
+			if( idx > 0 ) {
+				next = offset + idx;
+			}
+		}
+
 		return new IntTuple(previous, next);
 	}
 }
