bbbbbbdd3d8067b1e8315a61f795dd4a9e004f96
Tom Schindl <tom.schindl@bestsolution.at>
2016-04-12 21:00:10 +0200

    Bug 491535 - Add a validation story who is application to mvvm

---
+++ demos/org.eclipse.fx.ui.controls.sample/src/org/eclipse/fx/ui/controls/sample/ValidationVMSample.java
@@ -0,0 +1,87 @@
+package org.eclipse.fx.ui.controls.sample;
+
+import org.eclipse.fx.core.Status;
+import org.eclipse.fx.core.Status.State;
+import org.eclipse.fx.core.property.StatusAggregator;
+import org.eclipse.fx.core.property.ValidatedSimpleStringProperty;
+import org.eclipse.fx.core.property.ValidatedStringProperty;
+import org.eclipse.fx.ui.controls.form.DecoratedNodeBuilder;
+import org.eclipse.fx.ui.controls.form.StatusNode;
+import org.eclipse.fx.ui.panes.GridData;
+import org.eclipse.fx.ui.panes.GridData.Alignment;
+import org.eclipse.fx.ui.panes.GridLayoutPane;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ValidationVMSample extends Application {
+
+	static class VM {
+		private final ValidatedStringProperty firstname;
+		private final ValidatedStringProperty lastname;
+		private final StatusAggregator aggregator;
+
+		public VM(String firstname, String lastname) {
+			this.firstname = new ValidatedSimpleStringProperty(new SimpleStringProperty(this, "firstname",firstname));
+			this.firstname.validator(s -> s.isEmpty() ? Status.status(State.ERROR, -1, "Vorname muß befüllt sein", null) : Status.ok());
+
+			this.lastname = new ValidatedSimpleStringProperty(new SimpleStringProperty(this, "lastname", lastname));
+			this.lastname.validator(s -> s.isEmpty() ? Status.status(State.ERROR, -1, "Nachname muß befüllt sein", null) : Status.ok());
+
+			this.aggregator = new StatusAggregator(this.firstname,this.lastname);
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		GridLayoutPane p = new GridLayoutPane();
+		p.setNumColumns(2);
+
+		VM vm = new VM("Tom", "Schindl");
+
+		Label title = new Label("My Form");
+		title.setGraphic(new StatusNode(vm.aggregator.statusProperty()));
+		title.setStyle("-fx-font-size: 2em; -fx-font-weight: bold;");
+
+		GridLayoutPane.setConstraint(title, new GridData(Alignment.FILL,Alignment.CENTER,true,false,2,1));
+		p.getChildren().add(title);
+
+
+		{
+			Label l = new Label("Vorname");
+			p.getChildren().add(l);
+
+			TextField field = new TextField();
+			field.textProperty().bindBidirectional(vm.firstname.bindProperty());
+
+			p.getChildren().add(DecoratedNodeBuilder
+				.create(field, vm.firstname)
+				.decoratedNodeMutator(new GridData(GridData.FILL_HORIZONTAL), GridLayoutPane::setConstraint)
+				.build());
+		}
+
+		{
+			Label l = new Label("Nachname");
+			p.getChildren().add(l);
+
+			TextField field = new TextField();
+			field.textProperty().bindBidirectional(vm.lastname.bindProperty());
+
+			p.getChildren().add(DecoratedNodeBuilder
+					.create(field, vm.lastname)
+					.decoratedNodeMutator(new GridData(GridData.FILL_HORIZONTAL), GridLayoutPane::setConstraint)
+					.build());
+		}
+
+		primaryStage.setScene(new Scene(p,800,600));
+		primaryStage.show();
+	}
+}
