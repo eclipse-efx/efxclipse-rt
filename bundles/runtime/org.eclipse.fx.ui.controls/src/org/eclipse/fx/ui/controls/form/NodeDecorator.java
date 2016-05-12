949c75bcc4e66414fcf95985179af1867f96cb1a
Tom Schindl <tom.schindl@bestsolution.at>
2016-05-12 17:41:53 +0200

    Bug 493551 - NodeDecorator should support creating a decorator based on an ObservableValue

--- bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/form/NodeDecorator.java
+++ bundles/runtime/org.eclipse.fx.ui.controls/src/org/eclipse/fx/ui/controls/form/NodeDecorator.java
@@ -1,8 +1,10 @@
 package org.eclipse.fx.ui.controls.form;
 
+import org.eclipse.fx.core.Status;
 import org.eclipse.fx.core.Util;
 import org.eclipse.fx.core.property.ValidationStatusPropertyOwner;
 
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 
 public interface NodeDecorator {
@@ -22,5 +24,11 @@ public interface NodeDecorator {
 
 	public static void apply(NodeDecorator decorator, Node control, ValidationStatusPropertyOwner statusOwner) {
 		apply(decorator, control).statusProperty().bind(statusOwner.statusProperty());
+	}
+
+	public static DecoratedNode apply(Node control, ObservableValue<Status> property) {
+		DecoratedNode decoratedNode = apply(control);
+		decoratedNode.statusProperty().bind(property);
+		return decoratedNode;
 	}
 }
