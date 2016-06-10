e34dd05c97c971c148ad5fc21ba008154006bd57
Tom Schindl <tom.schindl@bestsolution.at>
2016-06-10 15:14:45 +0200

    Bug 418399 - Splash Screen does not disappear

---
+++ testcases/sample.splash.app/src/sample/splash/app/themes/DefaultTheme.java
@@ -0,0 +1,9 @@
+package sample.splash.app.themes;
+
+import org.eclipse.fx.ui.theme.AbstractTheme;
+
+public class DefaultTheme extends AbstractTheme {
+	public DefaultTheme() {
+		super("theme.default", "Default theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
+	}
+}
---
+++ testcases/sample.splash.app/src/sample/splash/app/themes/InteractiveSplashStartup.java
@@ -0,0 +1,31 @@
+package sample.splash.app.themes;
+
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.fx.ui.services.startup.StartupProgressTrackerService;
+import org.osgi.service.component.annotations.Component;
+
+import javafx.application.Platform;
+
+@Component(property="service.ranking:Integer=1")
+public class InteractiveSplashStartup implements StartupProgressTrackerService {
+	private IApplicationContext applicationContext;
+
+	@Override
+	public OSGiRV osgiApplicationLaunched(IApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+		System.err.println("launched");
+		return OSGiRV.CONTINUE;
+	}
+
+	@Override
+	public void stateReached(ProgressState state) {
+		if( state == DefaultProgressState.JAVAFX_INITIALIZED ) {
+//			Platform.setImplicitExit(false);
+			System.err.println("JavaFX initialized on FX-Thread");
+		} else if( state == DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD ) {
+			System.err.println("JavaFX initialized on Launcher Thread");
+			applicationContext.applicationRunning();
+		}
+	}
+
+}
