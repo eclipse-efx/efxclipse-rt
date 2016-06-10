d825507324eedb3e091721463d5fb541d3a91b53
Tom Schindl <tom.schindl@bestsolution.at>
2016-06-10 16:05:09 +0200

    Bug 418399 - Splash Screen does not disappear

--- bundles/runtime/org.eclipse.fx.ui.workbench.fx/src/org/eclipse/fx/ui/workbench/fx/E4MainThreadApplication.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.fx/src/org/eclipse/fx/ui/workbench/fx/E4MainThreadApplication.java
@@ -1,25 +1,80 @@
 package org.eclipse.fx.ui.workbench.fx;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.eclipse.fx.core.Util;
 import org.eclipse.fx.core.databinding.JFXRealm;
 import org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState;
 
 import com.sun.javafx.application.PlatformImpl;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
- * An E4Application who assumes to be launched at the same thread as the OSGi-Launcher
+ * An E4Application who assumes to be launched at the same thread as the
+ * OSGi-Launcher
  */
 @SuppressWarnings("restriction")
 public class E4MainThreadApplication extends E4Application {
+	static CountDownLatch STARTUP_LATCH = new CountDownLatch(1);
+	static CountDownLatch SHUTDOWN_LATCH = new CountDownLatch(1);
+
+	void super_launchE4JavaFxApplication() throws Exception {
+		super.launchE4JavaFxApplication();
+	}
+
 	@Override
 	protected void launchE4JavaFxApplication() throws Exception {
-		PlatformImpl.startup(() -> {
-			JFXRealm.createDefault();
-			if (Util.isMacOS()) {
-				updateStartupState(DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD);
-			}
+		if (Util.isMacOS()) {
+			PlatformImpl.startup(() -> {
+				JFXRealm.createDefault();
+				if (Util.isMacOS()) {
+					updateStartupState(DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD);
+				}
 
-			jfxStart(getApplicationContext(), null, null);
-		});
+				jfxStart(getApplicationContext(), null, null);
+			});
+		} else {
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					try {
+						super_launchE4JavaFxApplication();
+					} catch (Exception e) {
+						SHUTDOWN_LATCH.countDown();
+						e.printStackTrace();
+					}
+				}
+			};
+			t.start();
+
+			STARTUP_LATCH.await();
+			updateStartupState(DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD);
+			SHUTDOWN_LATCH.await();
+		}
+	}
+
+	@Override
+	protected Class<? extends Application> getJfxApplicationClass() {
+		return ExtendedE4Application.class;
+	}
+
+	/**
+	 * Extended application used on none OS-X system to appropriately handle
+	 * lifecycle semantics
+	 */
+	public static class ExtendedE4Application extends DefaultJFXApp {
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			STARTUP_LATCH.countDown();
+			super.start(primaryStage);
+		}
+
+		@Override
+		public void stop() throws Exception {
+			super.stop();
+			SHUTDOWN_LATCH.countDown();
+		}
 	}
 }
