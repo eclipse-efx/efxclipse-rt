27090815478e7b75c9f74e5231ca97f6ae51b137
Tom Schindl <tom.schindl@bestsolution.at>
2016-06-13 10:06:15 +0200

    Bug 418399 - Splash Screen does not disappear

--- bundles/runtime/org.eclipse.fx.ui.workbench.fx/src/org/eclipse/fx/ui/workbench/fx/services/InteractiveStartupService.java
+++ bundles/runtime/org.eclipse.fx.ui.workbench.fx/src/org/eclipse/fx/ui/workbench/fx/services/InteractiveStartupService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.fx.ui.workbench.fx.services;
 
 import java.net.URL;
@@ -19,6 +29,9 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
+/**
+ * Base class to implement a service to implement an interactive startup experience
+ */
 public abstract class InteractiveStartupService implements StartupProgressTrackerService {
 	private IApplicationContext applicationContext;
 	private Stage stage;
