d41a824957ae7b1b15e0d84ef5697cdf6b2daafb
Tom Schindl <tom.schindl@bestsolution.at>
2016-07-07 14:44:25 +0200

    Adding splash who only hides the screen after the workbench window is shown

---
+++ bundles/runtime/org.eclipse.fx.ui.workbench.fx/src/org/eclipse/fx/ui/workbench/fx/services/NativeSplashStartupService.java
@@ -0,0 +1,41 @@
+package org.eclipse.fx.ui.workbench.fx.services;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.fx.core.Util;
+import org.eclipse.fx.ui.services.startup.StartupProgressTrackerService;
+import org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.DefaultProgressState;
+import org.eclipse.fx.ui.services.startup.StartupProgressTrackerService.OSGiRV;
+
+public class NativeSplashStartupService implements StartupProgressTrackerService {
+	private IApplicationContext applicationContext;
+	private CountDownLatch hideLatch = new CountDownLatch(1);
+
+	@Override
+	public OSGiRV osgiApplicationLaunched(IApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+		return OSGiRV.CONTINUE;
+	}
+
+	@Override
+	public void stateReached(ProgressState state) {
+		if( state == DefaultProgressState.JAVAFX_INITIALIZED_LAUNCHER_THREAD ) {
+			if( ! Util.isMacOS() ) {
+				try {
+					this.hideLatch.await();
+				} catch (InterruptedException e) {
+					// nothing to do
+				}
+				this.applicationContext.applicationRunning();
+			}
+		} else if( state == DefaultProgressState.WORKBENCH_GUI_SHOWN ) {
+			if( ! Util.isMacOS() ) {
+				this.hideLatch.countDown();
+			} else {
+				this.applicationContext.applicationRunning();
+			}
+		}
+	}
+
+}
