ff898d08ee0d7df631bd14b2106be5497a88073f
Tom Schindl <tom.schindl@bestsolution.at>
2016-07-13 10:52:20 +0200

    Bug 497786 - Generify ui.databinding API
    * added new typed factory

--- bundles/runtime/org.eclipse.fx.ui.databinding/src/org/eclipse/fx/ui/databinding/JFXUIProperties.java
+++ bundles/runtime/org.eclipse.fx.ui.databinding/src/org/eclipse/fx/ui/databinding/JFXUIProperties.java
@@ -10,21 +10,22 @@
  *******************************************************************************/
 package org.eclipse.fx.ui.databinding;
 
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeTableView;
-import javafx.scene.control.TreeView;
-
 import java.time.LocalDate;
 
-import org.eclipse.fx.core.databinding.IJFXBeanValueProperty;
-import org.eclipse.fx.core.databinding.JFXBeanProperties;
 import org.eclipse.fx.ui.databinding.internal.DateValueProperty;
 import org.eclipse.fx.ui.databinding.internal.SingleSelectionProperty;
 import org.eclipse.fx.ui.databinding.internal.TextValueProperty;
 import org.eclipse.jdt.annotation.NonNull;
+
+import javafx.scene.control.Cell;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.control.TreeView;
 
 /**
  * Factory to create JavaFX properties
@@ -44,19 +45,43 @@ public class JFXUIProperties {
 	 */
 	@NonNull
 	public static <@NonNull S,T> IJFXControlValueProperty<S,T> singleViewSelection() {
-		return new SingleSelectionProperty<S,T>();
+		return new SingleSelectionProperty<>();
 	}
 
 	/**
-	 * Create a property for the text-property of a javafx bean
-	 *
-	 * @return the text property
-	 * @deprecated use {@link #text2()}
+	 * @return single selection property for {@link ListView}
+	 * @since 3.0.0
 	 */
 	@NonNull
-	@Deprecated
-	public static IJFXBeanValueProperty text() {
-		return JFXBeanProperties.value("text"); //$NON-NLS-1$
+	public static <T> IJFXControlValueProperty<ListView<T>,T> singleListViewSelection() {
+		return new SingleSelectionProperty<>();
+	}
+
+	/**
+	 * @return single selection property for {@link ComboBox}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static <T> IJFXControlValueProperty<ComboBox<T>,T> singleComboBoxSelection() {
+		return new SingleSelectionProperty<>();
+	}
+
+	/**
+	 * @return single selection property for {@link TreeView}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static <T> IJFXControlValueProperty<TreeTableView<T>,T> singleTreeTableViewSelection() {
+		return new SingleSelectionProperty<>();
+	}
+
+	/**
+	 * @return single selection property for {@link TreeView}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static <T> IJFXControlValueProperty<TreeView<T>,T> singleTreeViewSelection() {
+		return new SingleSelectionProperty<>();
 	}
 
 	/**
@@ -66,7 +91,43 @@ public class JFXUIProperties {
 	 * @since 2.4.0
 	 */
 	@NonNull
-	public static <@NonNull S> IJFXControlValueProperty<S,String> text2() {
+	public static <@NonNull S> IJFXControlValueProperty<S,String> text() {
+		return new TextValueProperty<>();
+	}
+
+	/**
+	 * @return text property for a {@link TextInputControl}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static IJFXControlValueProperty<TextInputControl,String> textInputText() {
+		return new TextValueProperty<>();
+	}
+
+	/**
+	 * @return text property of a {@link ComboBox#editorProperty()}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static IJFXControlValueProperty<ComboBox<?>,String> comboBoxText() {
+		return new TextValueProperty<>();
+	}
+
+	/**
+	 * @return text property of a {@link Cell}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static IJFXControlValueProperty<Cell<?>,String> cellText() {
+		return new TextValueProperty<>();
+	}
+
+	/**
+	 * @return text property of a {@link Tab}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static IJFXControlValueProperty<Tab,String> tabText() {
 		return new TextValueProperty<>();
 	}
 
@@ -77,6 +138,15 @@ public class JFXUIProperties {
 	 */
 	@NonNull
 	public static <@NonNull S> IJFXControlValueProperty<S,LocalDate> date() {
+		return new DateValueProperty<>();
+	}
+
+	/**
+	 * @return date property of a {@link DatePicker}
+	 * @since 3.0.0
+	 */
+	@NonNull
+	public static IJFXControlValueProperty<DatePicker,LocalDate> datePickerDate() {
 		return new DateValueProperty<>();
 	}
 }
