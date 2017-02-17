8ffc1d1d5e30f4c87ae88f9b9c88855785723247
Tom Schindl <tom.schindl@bestsolution.at>
2017-02-17 22:45:47 +0100

    move me baby

---
+++ modules/org.eclipse.fx.core/src/test/java/org/eclipse/fx/core/text/TestAnnotatedString.java
@@ -0,0 +1,20 @@
+package org.eclipse.fx.core.text;
+
+import org.eclipse.fx.core.text.AnnotatedString.Builder;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestAnnotatedString {
+	@Test
+	public void testAdd_Char() {
+		Builder<String> builder = AnnotatedString.create(String.class,10);
+		AnnotatedString<String> string = builder
+			.add(new char [] { 'H','e','l','l','o' }, "a", "b")
+			.add(new char[] { ' ' })
+			.add(new char [] { 'W','o','r','l','d', '!' }, "c", "d")
+			.build();
+		Assert.assertNotNull(string);
+		Assert.assertEquals(12, string.length());
+		Assert.assertEquals("Hello World!", string.toString());
+	}
+}
