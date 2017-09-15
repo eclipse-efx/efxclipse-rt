e2e6d38b5ec3e8745a11ff534b85f028150b6be8
Tom Schindl <tom.schindl@bestsolution.at>
2017-09-15 20:30:30 +0200

    fixes #159

---
+++ modules/ui/org.eclipse.fx.ui.workbench.base/src/main/java/org/eclipse/fx/ui/workbench/base/internal/EFX_ModelAssembler.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2017 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.fx.ui.workbench.base.internal;
+
+import java.util.Set;
+
+import org.eclipse.e4.ui.internal.workbench.ModelAssembler;
+import org.eclipse.e4.ui.internal.workbench.ModelFragmentComparator;
+import org.eclipse.e4.ui.internal.workbench.ModelFragmentWrapper;
+import org.eclipse.e4.ui.model.application.MApplicationElement;
+import org.eclipse.e4.ui.model.fragment.MStringModelFragment;
+import org.eclipse.e4.ui.model.internal.ModelUtils;
+
+/**
+ * Extended model assembler who fixes Bug 522361
+ */
+@SuppressWarnings("restriction")
+public class EFX_ModelAssembler extends ModelAssembler {
+	
+	@Override
+	public void processFragments(Set<ModelFragmentWrapper> fragmentList) {
+		super.processFragments(fragmentList);
+	}
+	
+	static class EFX_ModelFragmentComparator extends ModelFragmentComparator {
+		@Override
+		public int compare(ModelFragmentWrapper o1, ModelFragmentWrapper o2) {
+			if (o1 == o2) {
+				return 0;
+			}
+
+			if( o1.getModelFragment() instanceof MStringModelFragment && o2.getModelFragment() instanceof MStringModelFragment) {
+				MStringModelFragment sm1 = (MStringModelFragment) o1.getModelFragment();
+				MStringModelFragment sm2 = (MStringModelFragment) o2.getModelFragment();
+				
+				boolean hasElement = false;
+				for (MApplicationElement element : o2.getModelFragment().getElements()) {
+					hasElement |= ModelUtils.findElementById(element, sm1.getParentElementId()) != null;
+					if (hasElement)
+						break;
+				}
+				if (hasElement)
+					return 1;
+				hasElement = false;
+				for (MApplicationElement element : o1.getModelFragment().getElements()) {
+					hasElement |= ModelUtils.findElementById(element, sm2.getParentElementId()) != null;
+					if (hasElement)
+						break;
+				}
+				if (hasElement)
+					return -1;
+			}
+			
+			return super.compare(o1, o2);
+		}
+	}
+}
--- modules/ui/org.eclipse.fx.ui.workbench.base/src/main/java/org/eclipse/fx/ui/workbench/base/internal/EFX_ResourceHandler.java
+++ modules/ui/org.eclipse.fx.ui.workbench.base/src/main/java/org/eclipse/fx/ui/workbench/base/internal/EFX_ResourceHandler.java
@@ -186,7 +186,7 @@ public class EFX_ResourceHandler implements IModelResourceHandler {
 		MApplication appElement = (MApplication) this.resource.getContents().get(0);
 
 		this.context.set(MApplication.class, appElement);
-		ModelAssembler contribProcessor = ContextInjectionFactory.make(ModelAssembler.class,
+		ModelAssembler contribProcessor = ContextInjectionFactory.make(EFX_ModelAssembler.class,
 				this.context);
 		contribProcessor.processModel(initialModel);
 
