e473a3033533b1ae3a534c34e5b0a3a3a001e698
Tom Schindl <tom.schindl@bestsolution.at>
2018-01-31 19:40:42 +0100

    moved tests
    refs #182

--- modules/core/org.eclipse.fx.core.databinding/src/test/java/org/eclipse/fx/core/databinding/tests/AdapterFactoryTest.java
+++ modules/core/org.eclipse.fx.core.databinding/src/test/java/org/eclipse/fx/core/databinding/tests/AdapterFactoryTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.*;
  * 
  */
 @RunWith(RealmRunner.class)
-public class AdapterFactoryTests {
+public class AdapterFactoryTest {
 	
 	@Test
 	public void convertObservableList() {
