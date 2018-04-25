de39657b47633c9e7c6f8c1f239e4cc8dcee1d2c
Tom Schindl <tom.schindl@bestsolution.at>
2018-04-25 11:12:55 +0200

    added test-case
    refs #211

---
+++ modules/core/org.eclipse.fx.core.di.context.tests/src/org/eclipse/fx/core/di/context/tests/LoggerTestCase.java
@@ -0,0 +1,37 @@
+package org.eclipse.fx.core.di.context.tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.EclipseContextFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.fx.core.di.context.tests.LocalInstanceTestCase.TestBean;
+import org.eclipse.fx.core.log.FluentLogger;
+import org.eclipse.fx.core.log.Log;
+import org.eclipse.fx.core.log.Logger;
+import org.junit.Test;
+import org.osgi.framework.FrameworkUtil;
+
+public class LoggerTestCase {
+	public static class LogTestBean {
+		@Inject
+		@Log
+		Logger logger;
+		
+		@Inject
+		@Log
+		FluentLogger fLogger;
+	}
+	
+	@Test
+	public void testLogger() {
+		IEclipseContext serviceContext = EclipseContextFactory.getServiceContext(FrameworkUtil.getBundle(getClass()).getBundleContext());
+
+		IEclipseContext child = serviceContext.createChild();
+		LogTestBean bean = ContextInjectionFactory.make(LogTestBean.class, child);
+		assertNotNull(bean.logger);
+		assertNotNull(bean.fLogger);
+	}
+}
