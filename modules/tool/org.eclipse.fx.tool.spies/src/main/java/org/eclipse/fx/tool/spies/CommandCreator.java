26191a7b9ed0a45d797d4c1e6975e6efacc5c37b
Thomas Fahrmeyer <lauritziu_ii@yahoo.de>
2019-03-28 22:28:41 +0100

    added missing copyright headers
    Signed-off-by: Thomas Fahrmeyer <lauritziu_ii@yahoo.de>

--- modules/tool/org.eclipse.fx.tool.spies/src/main/java/org/eclipse/fx/tool/spies/CommandCreator.java
+++ modules/tool/org.eclipse.fx.tool.spies/src/main/java/org/eclipse/fx/tool/spies/CommandCreator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) Carl Zeiss Meditec AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Thomas Fahrmeyer (lauritziu_ii@yahoo.de) - Refactoring 
+*/
 package org.eclipse.fx.tool.spies;
 
 import org.eclipse.e4.ui.model.application.MApplication;
