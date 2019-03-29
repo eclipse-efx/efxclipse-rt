c15de051ec319b676c7d5b17bcffb63d2360c960
Thomas Fahrmeyer <lauritziu_ii@yahoo.de>
2019-03-29 11:09:05 +0100

    added missing copyright headers
    Signed-off-by: Thomas Fahrmeyer <lauritziu_ii@yahoo.de>

--- modules/tool/org.eclipse.fx.tool.context.spy/src/main/java/org/eclipse/fx/tool/context/spy/ContextSpyPart.java
+++ modules/tool/org.eclipse.fx.tool.context.spy/src/main/java/org/eclipse/fx/tool/context/spy/ContextSpyPart.java
@@ -1,4 +1,13 @@
- 
+/*******************************************************************************
+ * Copyright (c) Carl Zeiss Meditec AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Thomas Fahrmeyer (lauritziu_ii@yahoo.de) - initial API and implementation
+*/ 
 package org.eclipse.fx.tool.context.spy;
 
 import org.eclipse.fx.tool.context.spy.internal.model.ContextSpyModel;
@@ -10,9 +19,8 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import javax.annotation.PostConstruct;
 
 /**
- * Part of Context Spy
+ * Part implementation of Context Spy
  * 
- * @author x1jzimme
  */
 public class ContextSpyPart {
 
