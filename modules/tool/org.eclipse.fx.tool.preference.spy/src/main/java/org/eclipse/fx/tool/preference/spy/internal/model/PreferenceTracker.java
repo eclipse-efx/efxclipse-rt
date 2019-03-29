dd74cbc4cb40dfb78ebcb62a824937f0013e52ff
Thomas Fahrmeyer <lauritziu_ii@yahoo.de>
2019-03-29 12:01:07 +0100

    added missing copyright headers
    Signed-off-by: Thomas Fahrmeyer <lauritziu_ii@yahoo.de>

--- modules/tool/org.eclipse.fx.tool.preference.spy/src/main/java/org/eclipse/fx/tool/preference/spy/internal/model/PreferenceTracker.java
+++ modules/tool/org.eclipse.fx.tool.preference.spy/src/main/java/org/eclipse/fx/tool/preference/spy/internal/model/PreferenceTracker.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) Carl Zeiss Meditec AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Thomas Fahrmeyer (lauritziu_ii@yahoo.de) - initial API and implementation
+*/
 package org.eclipse.fx.tool.preference.spy.internal.model;
 
 import org.eclipse.fx.tool.preference.spy.internal.service.PreferenceService;
