9efbdf3e1928b1caa49df9663fb30f898d1067ac
Leo Rader <leo.rader@bestsolution.at>
2022-08-11 10:01:12 +0200

    updated copyright for EPL V2.0, refs #416

--- demos/CodeEditor/src/application/text/JavaSourceConfiguration.java
+++ demos/CodeEditor/src/application/text/JavaSourceConfiguration.java
@@ -1,11 +1,9 @@
 /*******************************************************************************
 * Copyright (c) 2014 BestSolution.at and others.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v.2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
 *
 * Contributors:
 * 	Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
--- experimental/swt/org.eclipse.fx.runtime.examples.swt/src-snippets/org/eclipse/swt/snippets/Snippet75.java
+++ experimental/swt/org.eclipse.fx.runtime.examples.swt/src-snippets/org/eclipse/swt/snippets/Snippet75.java
@@ -1,11 +1,9 @@
 ///*******************************************************************************
 // * Copyright (c) 2000, 2004 IBM Corporation and others.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v.2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
+// * All rights reserved. This program and the accompanying materials
+// * are made available under the terms of the Eclipse Public License v1.0
+// * which accompanies this distribution, and is available at
+// * http://www.eclipse.org/legal/epl-v10.html
 // *
 // * Contributors:
 // *     IBM Corporation - initial API and implementation
