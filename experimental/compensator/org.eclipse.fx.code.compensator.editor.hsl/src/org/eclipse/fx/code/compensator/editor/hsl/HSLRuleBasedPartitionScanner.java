5027ef2eea2b1f68e5f548797b839a5dc68a5ac2
Leo Rader <leo.rader@bestsolution.at>
2022-08-11 12:19:47 +0200

    added copyright for EPL V2.0, refs #416

--- experimental/compensator/org.eclipse.fx.code.compensator.editor.hsl/src/org/eclipse/fx/code/compensator/editor/hsl/HSLRuleBasedPartitionScanner.java
+++ experimental/compensator/org.eclipse.fx.code.compensator.editor.hsl/src/org/eclipse/fx/code/compensator/editor/hsl/HSLRuleBasedPartitionScanner.java
@@ -1,14 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2014 BestSolution.at and others.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v.2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
- * 
+ * Copyright (c) 2022 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.fx.code.compensator.editor.hsl;
 
--- modules/ui/org.eclipse.fx.ui.controls/src/main/java/org/eclipse/fx/ui/controls/scroll/ScrollView.java
+++ modules/ui/org.eclipse.fx.ui.controls/src/main/java/org/eclipse/fx/ui/controls/scroll/ScrollView.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2022 BestSolution.at and others.
+ *  
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v.2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *  
+ * SPDX-License-Identifier: EPL-2.0
+ *  
+ * Contributors:
+ *     BestSolution.at - initial API and implementation
+ *******************************************************************************/
 ///*******************************************************************************
 // * Copyright (c) 2019 BestSolution.at and others.
 // * 
