8d13142453e984f17a4f5bd410f308aab8e2846a
Leo Rader <leo.rader@bestsolution.at>
2022-08-17 15:49:28 +0200

    updated copyright for EPL V2.0, refs eclipse-efx#416
    Signed-off-by: Leo Rader <leo.rader@bestsolution.at>

--- modules/code/org.eclipse.fx.code.editor.fx/src/org/eclipse/fx/code/editor/fx/services/TextHoverMapProvider.java
+++ modules/code/org.eclipse.fx.code.editor.fx/src/org/eclipse/fx/code/editor/fx/services/TextHoverMapProvider.java
@@ -10,18 +10,6 @@
  * Contributors:
  *     BestSolution.at - initial API and implementation
  *******************************************************************************/
-///*******************************************************************************
-// * Copyright (c) 2015 BestSolution.at and others.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v.2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
-// *
-// * Contributors:
-// *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
-// *******************************************************************************/
 //package org.eclipse.fx.code.editor.fx.services;
 //
 //import org.eclipse.fx.code.editor.services.InputDependentTypeProviderService;
--- modules/ui/org.eclipse.fx.ui.databinding/src/main/java/org/eclipse/fx/ui/databinding/BindingSupport.java
+++ modules/ui/org.eclipse.fx.ui.databinding/src/main/java/org/eclipse/fx/ui/databinding/BindingSupport.java
@@ -10,18 +10,6 @@
  * Contributors:
  *     BestSolution.at - initial API and implementation
  *******************************************************************************/
-///*******************************************************************************
-// * Copyright (c) 2016 BestSolution.at and others.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v.2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
-// *
-// * Contributors:
-// *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
-// *******************************************************************************/
 //package org.eclipse.fx.ui.databinding;
 //
 //import org.eclipse.core.databinding.Binding;
